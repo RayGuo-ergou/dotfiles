25da4ebbb2261ab42ff8312bb4621c62c0406f02
Ray Guo <ma2808203259@hotmail.com>
2023-09-03 14:51:54 +1000

    refactor: shell script format

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -43,7 +43,7 @@ link_file () {
 
       # ignoring exit 1 from readlink in case where file already exists
       # shellcheck disable=SC2155
-      local currentSrc="$(readlink $dst)"
+      local currentSrc="$(readlink "$dst")"
 
       if [ "$currentSrc" == "$src" ]
       then
@@ -54,7 +54,7 @@ link_file () {
 
         user "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
         [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
-        read -n 1 action  < /dev/tty
+        read -rn 1 action  < /dev/tty
 
         case "$action" in
           o )
@@ -110,8 +110,8 @@ link_file () {
 prop () {
    PROP_KEY=$1
    PROP_FILE=$2
-   PROP_VALUE=$(eval echo "$(cat $PROP_FILE | grep "$PROP_KEY" | cut -d'=' -f2)")
-   echo $PROP_VALUE
+   PROP_VALUE=$(eval echo "$(cat "$PROP_FILE" | grep "$PROP_KEY" | cut -d'=' -f2)")
+   echo "$PROP_VALUE"
 }
 
 install_dotfiles () {
@@ -123,16 +123,17 @@ install_dotfiles () {
   # for linkfile in $(find -H "$DOTFILES" -maxdepth 2 -name 'links.prop' -not -path '*.git*')
   find -H "$DOTFILES" -maxdepth 2 -name 'links.prop' -not -path '*.git*' | while read -r linkfile;
   do
-    for line in $(cat "$linkfile");
+    # for line in $(cat "$linkfile");
+    while read -r line
     do
         local src dst dir
         src=$(eval echo "$line" | cut -d '=' -f 1)
         dst=$(eval echo "$line" | cut -d '=' -f 2)
-        dir=$(dirname $dst)
+        dir=$(dirname "$dst")
 
         mkdir -p "$dir"
         link_file "$src" "$dst"
-    done
+    done < "$linkfile"
   done
 }
 
@@ -140,7 +141,7 @@ create_env_file () {
     if test -f "$HOME/.env.sh"; then
         success "$HOME/.env.sh file already exists, skipping"
     else
-        echo "export DOTFILES=$DOTFILES" > $HOME/.env.sh
+        echo "export DOTFILES=$DOTFILES" > "$HOME"/.env.sh
         success 'created ~/.env.sh'
     fi
 }
