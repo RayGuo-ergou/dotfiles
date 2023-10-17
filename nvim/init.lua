f2091f469892aa5e8448182e24693b7383ffb801
Ray Guo <ma2808203259@hotmail.com>
2023-10-17 19:35:26 +1100

    feat: add neovim configs

---
+++ nvim/init.lua
@@ -0,0 +1,19 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- Operator Pending Mode Keybindings
+vim.api.nvim_set_keymap("o", "L", "$", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("o", "H", "^", { noremap = true, silent = true })
+
+-- Normal Mode Keybindings Non Recursive
+vim.api.nvim_set_keymap("n", "<C-n>", ":nohl<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>c", "caw", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>h", "^", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>l", "$", { noremap = true, silent = true })
+
+-- Visual Mode Keybindings
+vim.api.nvim_set_keymap("v", "<leader>", "<esc>", { noremap = true, silent = true })
+
+-- Insert Mode Keybindings
+vim.api.nvim_set_keymap("i", "jj", "<esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-n>", "<esc>:nohl<CR>", { noremap = true, silent = true })
