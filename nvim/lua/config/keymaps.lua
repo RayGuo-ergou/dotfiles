c5f3a11aa86b668c30b038ef734149da68ac5fc4
Ray Guo <ma2808203259@hotmail.com>
2023-10-18 13:15:25 +1100

    refactor(neovim): use vim.keymap.set which is a wrap of nvim_set_key_map

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,19 +3,19 @@
 -- Add any additional keymaps here
 
 -- Operator Pending Mode Keybindings
-vim.api.nvim_set_keymap("o", "L", "$", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("o", "H", "^", { noremap = true, silent = true })
+vim.keymap.set("o", "L", "$", { noremap = true, silent = true })
+vim.keymap.set("o", "H", "^", { noremap = true, silent = true })
 
 -- Normal Mode Keybindings Non Recursive
-vim.api.nvim_set_keymap("n", "<C-n>", ":nohl<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>c", "caw", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>h", "^", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>l", "$", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
+vim.keymap.set("n", "<C-n>", ":nohl<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>c", "caw", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>h", "^", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>l", "$", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
 -- Visual Mode Keybindings
-vim.api.nvim_set_keymap("v", "<leader>", "<esc>", { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>", "<esc>", { noremap = true, silent = true })
 
 -- Insert Mode Keybindings
-vim.api.nvim_set_keymap("i", "jj", "<esc>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<C-n>", "<esc>:nohl<CR>", { noremap = true, silent = true })
+vim.keymap.set("i", "jj", "<esc>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-n>", "<esc>:nohl<CR>", { noremap = true, silent = true })
