676c3120bd0cff3fa735b27ca3eaea712a89c30c
Ray Guo <ma2808203259@hotmail.com>
2024-01-16 09:55:34 +1100

    feat(nvim): key mapping

--- nvim/lua/ergou/config/keymaps.lua
+++ nvim/lua/ergou/config/keymaps.lua
@@ -2,29 +2,111 @@
 vim.g.mapleader = " "
 
 -- key mapping
-local keymap = vim.keymap -- for conciseness
+local map = vim.keymap -- for conciseness
 
 -- Operator Pending Mode Keybindings
-keymap.set("o", "L", "$", { noremap = true, silent = true })
-keymap.set("o", "H", "^", { noremap = true, silent = true })
+map.set("o", "L", "$", { noremap = true, silent = true })
+map.set("o", "H", "^", { noremap = true, silent = true })
 
 -- Normal Mode Keybindings Non Recursive
-keymap.set("n", "<C-n>", ":nohl<CR>", { noremap = true, silent = true })
-keymap.set("n", "<leader>c", "caw", { noremap = true, silent = true })
-keymap.set("n", "<leader>h", "^", { noremap = true, silent = true })
-keymap.set("n", "<leader>l", "$", { noremap = true, silent = true })
-keymap.set("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
-keymap.set("n", "1", "*", { noremap = true, silent = true })
-keymap.set("n", "2", "#", { noremap = true, silent = true })
+map.set("n", "<C-n>", ":nohl<CR>", { noremap = true, silent = true })
+map.set("n", "<leader>c", "caw", { noremap = true, silent = true })
+map.set("n", "<leader>h", "^", { noremap = true, silent = true })
+map.set("n", "<leader>l", "$", { noremap = true, silent = true })
+map.set("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
+map.set("n", "1", "*", { noremap = true, silent = true })
+map.set("n", "2", "#", { noremap = true, silent = true })
 
 -- Visual Mode Keybindings
-keymap.set("v", "<leader>", "<esc>", { noremap = true, silent = true })
-keymap.set("v", "L", "$", { noremap = true, silent = true })
-keymap.set("v", "H", "^", { noremap = true, silent = true })
+map.set("v", "<leader>", "<esc>", { noremap = true, silent = true })
+map.set("v", "L", "$", { noremap = true, silent = true })
+map.set("v", "H", "^", { noremap = true, silent = true })
 
 -- Insert Mode Keybindings
-keymap.set("i", "jj", "<esc>", { noremap = true, silent = true })
-keymap.set("i", "<C-n>", "<esc>:nohl<CR>", { noremap = true, silent = true })
+map.set("i", "jj", "<esc>", { noremap = true, silent = true })
+map.set("i", "<C-n>", "<esc>:nohl<CR>", { noremap = true, silent = true })
 
 -- save file
-keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+map.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+
+-- better up/down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+
+-- Move Lines
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+
+-- buffers
+map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next search result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev search result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+
+-- Add undo break-points
+map("i", ",", ",<c-g>u")
+map("i", ".", ".<c-g>u")
+map("i", ";", ";<c-g>u")
+
+-- better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- new file
+map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
+
+map("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
+map("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
+
+map("n", "[q", vim.cmd.cprev, { desc = "Previous quickfix" })
+map("n", "]q", vim.cmd.cnext, { desc = "Next quickfix" })
+
+-- quit
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+
+-- diagnostic
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+
+-- highlights under cursor
+map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
