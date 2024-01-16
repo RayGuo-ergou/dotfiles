f827da4c5d89c833098c13129a8712d63416983f
Ray Guo <ma2808203259@hotmail.com>
2024-01-16 22:48:12 +1100

    feat(nvim): change keybind for all files

--- nvim/lua/ergou/plugins/telescope.lua
+++ nvim/lua/ergou/plugins/telescope.lua
@@ -80,7 +80,7 @@ return {
     vim.keymap.set("n", "<leader>s/", telescope_live_grep_open_files, { desc = "[S]earch [/] in Open Files" })
     vim.keymap.set("n", "<leader>ss", require("telescope.builtin").builtin, { desc = "[S]earch [S]elect Telescope" })
     vim.keymap.set("n", "<leader>sf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
-    vim.keymap.set("n", "<leader>af", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
+    vim.keymap.set("n", "<leader>sa", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
     vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
     vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
     vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
