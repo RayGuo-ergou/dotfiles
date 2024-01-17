5462567baf98e28388b22421e35ce7f7665106d1
Ray Guo <ma2808203259@hotmail.com>
2024-01-17 16:44:30 +1100

    feat(nvim): add arg for live grep

--- nvim/lua/ergou/plugins/telescope.lua
+++ nvim/lua/ergou/plugins/telescope.lua
@@ -5,6 +5,12 @@ return {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
+    },
   },
   config = function()
     local telescope = require("telescope")
@@ -23,6 +29,7 @@ return {
     })
 
     telescope.load_extension("fzf")
+    telescope.load_extension("live_grep_args")
 
     -- Telescope live_grep in git root
     -- Function to find the git root directory based on the current buffer's path
@@ -41,7 +48,7 @@ return {
 
       -- Find the Git root directory from the current file's path
       local git_root =
-        vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
+          vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
       if vim.v.shell_error ~= 0 then
         print("Not a git repository. Searching on current working directory")
         return cwd
@@ -87,5 +94,6 @@ return {
     vim.keymap.set("n", "<leader>sG", ":LiveGrepGitRoot<cr>", { desc = "[S]earch by [G]rep on Git Root" })
     vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
     vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
+    vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
   end,
 }
