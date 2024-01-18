317e98228c80159423be854bc97f2a66a0a3d473
Ray Guo <ma2808203259@hotmail.com>
2024-01-18 21:49:50 +1100

    feat(nvim): highlight on yank

--- nvim/lua/ergou/config/autocmd.lua
+++ nvim/lua/ergou/config/autocmd.lua
@@ -1,3 +1,15 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup("ergou_" .. name, { clear = true })
+end
+
 -- Disable auto comment after a comment
 vim.cmd("autocmd BufEnter * set formatoptions-=cro")
 vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
