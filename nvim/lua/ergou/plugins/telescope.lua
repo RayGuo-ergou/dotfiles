c36ac6f886a2ffd558922646fd71657ea8db609c
Ray Guo <ma2808203259@hotmail.com>
2024-01-23 13:54:32 +1100

    feat(nvim): telescope search recent

--- nvim/lua/ergou/plugins/telescope.lua
+++ nvim/lua/ergou/plugins/telescope.lua
@@ -87,6 +87,7 @@ return {
     vim.keymap.set("n", "<leader>s/", telescope_live_grep_open_files, { desc = "[S]earch [/] in Open Files" })
     vim.keymap.set("n", "<leader>ss", require("telescope.builtin").builtin, { desc = "[S]earch [S]elect Telescope" })
     vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
+    vim.keymap.set("n", "<Leader>fr", require("telescope.builtin").oldfiles, { desc = "[S]earch [R]ecent" })
     vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
     vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
     vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
