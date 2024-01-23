68922fc61b8d7328e7b2e65762549e7efaac4037
Ray Guo <ma2808203259@hotmail.com>
2024-01-24 09:42:41 +1100

    feat(nvim): remove replace with register

--- nvim/lua/ergou/plugins/init.lua
+++ nvim/lua/ergou/plugins/init.lua
@@ -3,7 +3,6 @@ return {
 
   { "MunifTanjim/nui.nvim", lazy = true }, -- Many UI depends on
 
-  "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
   -- Git related plugins
   "tpope/vim-fugitive",
   "tpope/vim-rhubarb",
