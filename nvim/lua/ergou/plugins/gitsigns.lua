dca3788201d71f3675656cf4e6cd6331432e3ff0
Ray Guo <ma2808203259@hotmail.com>
2024-02-06 21:20:19 +1100

    feat(nvim): change gitsigns icons

--- nvim/lua/ergou/plugins/gitsigns.lua
+++ nvim/lua/ergou/plugins/gitsigns.lua
@@ -5,11 +5,12 @@ return {
     opts = {
       -- See `:help gitsigns.txt`
       signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
       },
       on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
