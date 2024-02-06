1f2a9b5cc81fb5090c721f7e9858b01b0af2cb6a
Ray Guo <ma2808203259@hotmail.com>
2024-02-06 23:26:34 +1100

    feat(nvim): diffview config

--- nvim/lua/ergou/plugins/diffview.lua
+++ nvim/lua/ergou/plugins/diffview.lua
@@ -1,1 +1,19 @@
-return { "sindrets/diffview.nvim" }
+return {
+  "sindrets/diffview.nvim",
+  opts = {
+    enhanced_diff_hl = true,
+  },
+  keys = {
+    {
+      "<leader>dv",
+      function()
+        if next(require("diffview.lib").views) == nil then
+          vim.cmd("DiffviewOpen")
+        else
+          vim.cmd("DiffviewClose")
+        end
+      end,
+      desc = "Toggle Diffview",
+    },
+  },
+}
