f0f58def44f83a11915dcdcf0948252437ea35b1
Ray Guo <ma2808203259@hotmail.com>
2024-02-07 14:54:06 +1100

    feat(nvim): remove no continuously comment

--- nvim/lua/ergou/config/autocmd.lua
+++ nvim/lua/ergou/config/autocmd.lua
@@ -2,10 +2,6 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("ergou_" .. name, { clear = true })
 end
 
--- Disable auto comment after a comment
-vim.cmd("autocmd BufEnter * set formatoptions-=cro")
-vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
-
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = augroup("highlight_yank"),
