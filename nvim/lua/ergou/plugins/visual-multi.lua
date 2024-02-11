09d203f644d5156f5bd6a411cde5de62eac419c1
Ray Guo <ma2808203259@hotmail.com>
2024-02-11 22:50:28 +1100

    feat(nvim): add multi select
    TODO: Adding more keys, for only one select under word

---
+++ nvim/lua/ergou/plugins/visual-multi.lua
@@ -0,0 +1,10 @@
+return {
+  'mg979/vim-visual-multi',
+  branch = 'master',
+  init = function()
+    vim.g.VM_default_mappings = 0
+    vim.g.VM_maps = {
+      ['Find Under'] = '<C-d>',
+    }
+  end,
+}
