c66dad5cd483389b95bf986af0097a40180c1517
Ray Guo <ma2808203259@hotmail.com>
2024-02-17 21:48:38 +1100

    feat(nvim): restructure treesitter

--- nvim/lua/ergou/plugins/nvim-treesitter.lua
+++ nvim/lua/ergou/plugins/nvim-treesitter.lua
@@ -6,108 +6,122 @@ return {
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
     build = ':TSUpdate',
-    config = function()
-      -- [[ Configure Treesitter ]]
-      -- See `:help nvim-treesitter`
-      -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
-      vim.defer_fn(function()
-        require('nvim-treesitter.configs').setup({
-          -- Add languages to be installed here that you want installed for treesitter
-          ensure_installed = {
-            'c',
-            'diff',
-            'cpp',
-            'go',
-            'lua',
-            'python',
-            'rust',
-            'tsx',
-            'javascript',
-            'typescript',
-            'vimdoc',
-            'vim',
-            'bash',
-            'vim',
-            'html',
-            'css',
-            'scss',
-            'vue',
-            'php',
-            'phpdoc',
-            'json',
-            'jsonc',
-            'json5',
-            'jsdoc',
-            'http',
-          },
+    opts = {
+      -- Add languages to be installed here that you want installed for treesitter
+      ensure_installed = {
+        'c',
+        'diff',
+        'cpp',
+        'go',
+        'lua',
+        'luadoc',
+        'luap',
+        'python',
+        'rust',
+        'tsx',
+        'javascript',
+        'typescript',
+        'vimdoc',
+        'vim',
+        'bash',
+        'html',
+        'css',
+        'scss',
+        'vue',
+        'php',
+        'phpdoc',
+        'json',
+        'jsonc',
+        'json5',
+        'jsdoc',
+        'http',
+        'regex',
+        'toml',
+        'yaml',
+        'query',
+      },
 
-          -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-          auto_install = false,
-          -- Install languages synchronously (only applied to `ensure_installed`)
-          sync_install = false,
-          -- List of parsers to ignore installing
-          ignore_install = {},
-          -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
-          modules = {},
-          highlight = { enable = true },
-          indent = { enable = true },
-          incremental_selection = {
-            enable = true,
-            keymaps = {
-              init_selection = '<c-space>',
-              node_incremental = '<c-space>',
-              scope_incremental = '<c-s>',
-              node_decremental = '<M-space>',
-            },
+      -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+      auto_install = false,
+      -- Install languages synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+      -- List of parsers to ignore installing
+      ignore_install = {},
+      -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
+      modules = {},
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<c-space>',
+          node_incremental = '<c-space>',
+          scope_incremental = '<c-s>',
+          node_decremental = '<M-space>',
+        },
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ['aa'] = '@parameter.outer',
+            ['ia'] = '@parameter.inner',
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
           },
-          textobjects = {
-            select = {
-              enable = true,
-              lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-              keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                ['aa'] = '@parameter.outer',
-                ['ia'] = '@parameter.inner',
-                ['af'] = '@function.outer',
-                ['if'] = '@function.inner',
-                ['ac'] = '@class.outer',
-                ['ic'] = '@class.inner',
-              },
-            },
-            move = {
-              enable = true,
-              set_jumps = true, -- whether to set jumps in the jumplist
-              goto_next_start = {
-                [']f'] = '@function.outer',
-                [']c'] = '@class.outer',
-              },
-              goto_next_end = {
-                [']F'] = '@function.outer',
-                [']C'] = '@class.outer',
-              },
-              goto_previous_start = {
-                ['[f'] = '@function.outer',
-                ['[c'] = '@class.outer',
-              },
-              goto_previous_end = {
-                ['[F'] = '@function.outer',
-                ['[C'] = '@class.outer',
-              },
-            },
-            swap = {
-              enable = true,
-              swap_next = {
-                ['<leader>sa'] = '@parameter.inner',
-              },
-              swap_previous = {
-                ['<leader>sA'] = '@parameter.inner',
-              },
-            },
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            [']f'] = '@function.outer',
+            [']c'] = '@class.outer',
           },
-        })
-      end, 0)
+          goto_next_end = {
+            [']F'] = '@function.outer',
+            [']C'] = '@class.outer',
+          },
+          goto_previous_start = {
+            ['[f'] = '@function.outer',
+            ['[c'] = '@class.outer',
+          },
+          goto_previous_end = {
+            ['[F'] = '@function.outer',
+            ['[C'] = '@class.outer',
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ['<leader>sa'] = '@parameter.inner',
+          },
+          swap_previous = {
+            ['<leader>sA'] = '@parameter.inner',
+          },
+        },
+      },
+    },
+    ---@param opts TSConfig
+    config = function(_, opts)
+      if type(opts.ensure_installed) == 'table' then
+        ---@type table<string, boolean>
+        local added = {}
+        opts.ensure_installed = vim.tbl_filter(function(lang)
+          if added[lang] then
+            return false
+          end
+          added[lang] = true
+          return true
+        end, opts.ensure_installed)
+      end
+      require('nvim-treesitter.configs').setup(opts)
     end,
   },
+
   -- Automatically add closing tags for HTML and JSX
   {
     'windwp/nvim-ts-autotag',
