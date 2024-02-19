b5a7ec6ca87f3342786b813260dfad6fc77d1434
Ray Guo <ma2808203259@hotmail.com>
2024-02-19 17:46:14 +1100

    feat(nvim): lazy load TS and few more config
    steal more code from folke

--- nvim/lua/ergou/plugins/nvim-treesitter.lua
+++ nvim/lua/ergou/plugins/nvim-treesitter.lua
@@ -1,13 +1,54 @@
 return {
   {
-    -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
     build = ':TSUpdate',
+    event = { 'LazyFile', 'VeryLazy' },
+    init = function(plugin)
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treeitter** module to be loaded in time.
+      -- Luckily, the only thins that those plugins need are the custom queries, which we make available
+      -- during startup.
+      require('lazy.core.loader').add_to_rtp(plugin)
+      require('nvim-treesitter.query_predicates')
+    end,
+    dependencies = {
+      {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        config = function()
+          -- When in diff mode, we want to use the default
+          -- vim text objects c & C instead of the treesitter ones.
+          local move = require('nvim-treesitter.textobjects.move') ---@type table<string,fun(...)>
+          local configs = require('nvim-treesitter.configs')
+          for name, fn in pairs(move) do
+            if name:find('goto') == 1 then
+              move[name] = function(q, ...)
+                if vim.wo.diff then
+                  local config = configs.get_module('textobjects.move')[name] ---@type table<string,string>
+                  for key, query in pairs(config or {}) do
+                    if q == query and key:find('[%]%[][cC]') then
+                      vim.cmd('normal! ' .. key)
+                      return
+                    end
+                  end
+                end
+                return fn(q, ...)
+              end
+            end
+          end
+        end,
+      },
+    },
+    cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall' },
+    keys = {
+      { '<c-space>', desc = 'Increment selection' },
+      { '<bs>', desc = 'Decrement selection', mode = 'x' },
+    },
+    ---@type TSConfig
+    ---@diagnostic disable-next-line: missing-fields
     opts = {
-      -- Add languages to be installed here that you want installed for treesitter
+      highlight = { enable = true },
+      indent = { enable = true },
       ensure_installed = {
         'c',
         'diff',
@@ -40,24 +81,13 @@ return {
         'yaml',
         'query',
       },
-
-      -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-      auto_install = false,
-      -- Install languages synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-      -- List of parsers to ignore installing
-      ignore_install = {},
-      -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
-      modules = {},
-      highlight = { enable = true },
-      indent = { enable = true },
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = '<c-space>',
-          node_incremental = '<c-space>',
-          scope_incremental = '<c-s>',
-          node_decremental = '<M-space>',
+          init_selection = '<C-space>',
+          node_incremental = '<C-space>',
+          scope_incremental = false,
+          node_decremental = '<bs>',
         },
       },
       textobjects = {
@@ -76,23 +106,10 @@ return {
         },
         move = {
           enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            [']f'] = '@function.outer',
-            [']c'] = '@class.outer',
-          },
-          goto_next_end = {
-            [']F'] = '@function.outer',
-            [']C'] = '@class.outer',
-          },
-          goto_previous_start = {
-            ['[f'] = '@function.outer',
-            ['[c'] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[F'] = '@function.outer',
-            ['[C'] = '@class.outer',
-          },
+          goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer' },
+          goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer' },
+          goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer' },
+          goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer' },
         },
         swap = {
           enable = true,
@@ -125,6 +142,7 @@ return {
   -- Automatically add closing tags for HTML and JSX
   {
     'windwp/nvim-ts-autotag',
+    event = 'LazyFile',
     opts = {},
   },
 }
