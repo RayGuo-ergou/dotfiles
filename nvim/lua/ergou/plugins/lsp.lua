38859fc64b42e6d319c1307e64672e77aebcbc6b
Ray Guo <ma2808203259@hotmail.com>
2024-02-21 21:09:10 +1100

    revert(nvim): revert keybind

--- nvim/lua/ergou/plugins/lsp.lua
+++ nvim/lua/ergou/plugins/lsp.lua
@@ -59,8 +59,8 @@ return {
         nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
         -- See `:help K` for why this keymap
-        nmap('<C-h>', vim.lsp.buf.hover, 'Hover Documentation')
-        nmap('<leader>h', vim.lsp.buf.signature_help, 'Signature Documentation')
+        nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+        nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
         -- Lesser used LSP functionality
         nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
