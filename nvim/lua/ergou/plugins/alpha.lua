7a555ed6231c4d495f0231f1a2b35530b70a1c55
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-02-22 21:52:43 +1100

    feat(nvim): add persistence

--- nvim/lua/ergou/plugins/alpha.lua
+++ nvim/lua/ergou/plugins/alpha.lua
@@ -26,6 +26,8 @@ return {
       dashboard.button("f", " " .. " Find file",       "<cmd> Telescope find_files <cr>"),
       dashboard.button("n", " " .. " New file",        "<cmd> ene <BAR> startinsert <cr>"),
       dashboard.button("r", " " .. " Recent files",    "<cmd> Telescope oldfiles <cr>"),
+      dashboard.button("c", " " .. " Config",          "<cmd> lua require('ergou.util').telescope.config_files()() <cr>"),
+      dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
       dashboard.button("g", " " .. " Find text",       "<cmd> Telescope live_grep <cr>"),
       dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
       dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
