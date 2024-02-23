2d2d1a158b2ce0ac9b44ef5e9eb5bf9cd0844487
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-02-23 19:46:43 +1100

    feat(tmux): tmux nvim window aware jump

--- nvim/lua/ergou/plugins/tmux.lua
+++ nvim/lua/ergou/plugins/tmux.lua
@@ -8,9 +8,10 @@ return {
     'TmuxNavigatePrevious',
   },
   keys = {
-    { '<m-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-    { '<m-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-    { '<m-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-    { '<m-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+    { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
   },
 }
