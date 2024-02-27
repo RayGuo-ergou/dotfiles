6b8ae085068150e52bbedd4fe8890b65cced2275
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-02-27 20:17:33 +1100

    chore(nvim): fix a typo

--- nvim/lua/ergou/plugins/typescript.lua
+++ nvim/lua/ergou/plugins/typescript.lua
@@ -1,5 +1,5 @@
 return {
   -- NOTE: add tsx filetype if doing react
-  { 'dmmulroy/ts-error-translator.nvim', opts = {}, ft = { 'typesctipe', 'vue' } },
+  { 'dmmulroy/ts-error-translator.nvim', opts = {}, ft = { 'typescript', 'vue' } },
   { 'dmmulroy/tsc.nvim', opts = {}, cmd = { 'TSC' } },
 }
