e0e79adbf23a78ec7871cd19dfd267fc67b6ace2
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-02-28 22:06:56 +1100

    chore(zsh): remove google cloud sdk

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -28,12 +28,6 @@ fi
 unset __conda_setup
 # <<< conda initialize <<<
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '$HOME/google-cloud-sdk/path.zsh.inc' ]; then . '$HOME/google-cloud-sdk/path.zsh.inc'; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f '$HOME/google-cloud-sdk/completion.zsh.inc' ]; then . '$HOME/google-cloud-sdk/completion.zsh.inc'; fi
-
 # Init sdkman
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 
