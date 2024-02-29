54f592e9645dec3d54c473d6b350c57287e8f6a3
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-02-29 19:15:52 +1100

    feat(nvim): disable notify
    it caused cursor blink ( not caused by it but something related to it
    that i am not sure for now ). Just disabled it for now

--- nvim/lua/ergou/plugins/ui.lua
+++ nvim/lua/ergou/plugins/ui.lua
@@ -43,10 +43,11 @@ return {
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      'rcarriga/nvim-notify',
+      -- 'rcarriga/nvim-notify',
     },
   },
   {
+    enabled = false,
     'rcarriga/nvim-notify',
     keys = {
       {
