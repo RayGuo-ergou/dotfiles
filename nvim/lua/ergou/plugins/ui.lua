f1a27b90aebb818d751e9970b4afb30adde0cc70
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-02-29 19:20:33 +1100

    Revert "feat(nvim): remove aerial"
    This reverts commit f4fae422a5abe87686fa03d244167a97b9a7ea7f.

--- nvim/lua/ergou/plugins/ui.lua
+++ nvim/lua/ergou/plugins/ui.lua
@@ -71,4 +71,25 @@ return {
       end,
     },
   },
+  {
+    'stevearc/aerial.nvim',
+    opts = {},
+    keys = {
+      {
+        '<leader>a',
+        '<cmd>AerialToggle!<CR>',
+        desc = 'Toggle Aerial',
+      },
+      {
+        '{',
+        '<cmd>AerialPrev<CR>',
+        desc = 'Previous Aerial',
+      },
+      {
+        '}',
+        '<cmd>AerialNext<CR>',
+        desc = 'Next Aerial',
+      },
+    },
+  },
 }
