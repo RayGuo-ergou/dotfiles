b2874232d59bfb3235d654d2415accf4bba82814
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-03-05 22:09:14 +1100

    fix(nvim): fix type

--- nvim/lua/ergou/util/lsp.lua
+++ nvim/lua/ergou/util/lsp.lua
@@ -1,14 +1,14 @@
 local M = {}
 
 function M.get_clients(opts)
-  local ret = {} ---@type lsp.Client[]
+  local ret = {} ---@type vim.lsp.Client[]
   if vim.lsp.get_clients then
     ret = vim.lsp.get_clients(opts)
   else
     ---@diagnostic disable-next-line: deprecated
     ret = vim.lsp.get_active_clients(opts)
     if opts and opts.method then
-      ---@param client lsp.Client
+      ---@param client vim.lsp.Client
       ret = vim.tbl_filter(function(client)
         return client.supports_method(opts.method, { bufnr = opts.bufnr })
       end, ret)
