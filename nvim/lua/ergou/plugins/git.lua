349d3db6cacba40a5e8c9ec3ec7ddd03cddbc77c
ray guo <ma2808203259@hotmail.com>
2024-03-16 13:39:00 +1100

    feat(nvim): add lazygit intergration

--- nvim/lua/ergou/plugins/git.lua
+++ nvim/lua/ergou/plugins/git.lua
@@ -84,4 +84,14 @@ return {
       },
     },
   },
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+  },
 }
