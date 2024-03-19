a2c7ac69c0095df3c150ca9d0e229329d4ab30c2
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-03-19 11:42:14 +1100

    feat(nvim): bump reset.nvim to v2

--- nvim/lua/ergou/plugins/rest.lua
+++ nvim/lua/ergou/plugins/rest.lua
@@ -1,61 +1,34 @@
 return {
-  'rest-nvim/rest.nvim',
-  -- Lazy load otherwise it will break tree sitter
-  ft = 'http',
-  config = function()
-    require('rest-nvim').setup({
-      -- Open request results in a horizontal split
-      result_split_horizontal = false,
-      -- Keep the http file buffer above|left when split horizontal|vertical
-      result_split_in_place = false,
-      -- stay in current windows (.http file) or change to results window (default)
-      stay_in_current_window_after_split = false,
-      -- Skip SSL verification, useful for unknown certificates
-      skip_ssl_verification = false,
-      -- Encode URL before making request
-      encode_url = true,
-      -- Highlight request on run
-      highlight = {
-        enabled = true,
-        timeout = 150,
+  {
+    'rest-nvim/rest.nvim',
+    -- Lazy load otherwise it will break tree sitter
+    ft = 'http',
+    dependencies = {
+      -- If having issue with install lua-curl (curl.h not found)
+      -- Use sudo ln -s /usr/include/x86_64-linux-gnu/curl /usr/include/curl
+      {
+        'vhyrro/luarocks.nvim',
+        config = function()
+          require('luarocks').setup({ rocks = { 'lua-curl', 'nvim-nio', 'mimetypes', 'xml2lua' } })
+        end,
       },
-      result = {
-        -- toggle showing URL, HTTP info, headers at top the of result window
-        show_url = true,
-        -- show the generated curl command in case you want to launch
-        -- the same request via the terminal (can be verbose)
-        show_curl_command = false,
-        show_http_info = true,
-        show_headers = true,
-        -- table of curl `--write-out` variables or false if disabled
-        -- for more granular control see Statistics Spec
-        show_statistics = false,
-        -- executables or functions for formatting response body [optional]
-        -- set them to false if you want to disable them
-        formatters = {
-          json = 'jq',
-          html = function(body)
-            return vim.fn.system({ 'tidy', '-i', '-q', '-' }, body)
-          end,
-        },
-      },
-      -- Jump to request line on run
-      jump_to_request = false,
-      env_file = 'http.env',
-    })
-  end,
-  keys = {
-    {
-      '<leader>cr',
-      '<Plug>RestNvim',
-      mode = { 'n', 'x' },
-      desc = '[C]url [R]equest',
     },
-    {
-      '<leader>cp',
-      '<Plug>RestNvimPreview',
-      mode = { 'n', 'x' },
-      desc = '[C]url [P]review',
+    config = function()
+      require('rest-nvim').setup()
+    end,
+    keys = {
+      {
+        '<leader>cr',
+        '<Plug>RestNvim',
+        mode = { 'n', 'x' },
+        desc = '[C]url [R]equest',
+      },
+      {
+        '<leader>cp',
+        '<Plug>RestNvimPreview',
+        mode = { 'n', 'x' },
+        desc = '[C]url [P]review',
+      },
     },
   },
 }
