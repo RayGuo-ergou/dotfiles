01c06d1615dfd3e8b4bfe6e7c3b70b6d0eaf4b6a
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-03-20 22:54:41 +1100

    chore(nvim): remove version and branch from plugin config

--- nvim/lua/ergou/plugins/surround.lua
+++ nvim/lua/ergou/plugins/surround.lua
@@ -1,7 +1,6 @@
 return {
   {
     'kylechui/nvim-surround',
-    version = '*', -- Use for stability; omit to use `main` branch for the latest features
     event = 'LazyFile',
     config = function()
       require('nvim-surround').setup()
--- nvim/lua/ergou/plugins/visual-multi.lua
+++ nvim/lua/ergou/plugins/visual-multi.lua
@@ -1,6 +1,5 @@
 return {
   'mg979/vim-visual-multi',
-  branch = 'master',
   event = 'LazyFile',
   init = function()
     vim.g.VM_default_mappings = 0
