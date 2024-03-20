c495cb683d823f7aa532778d01458451c6e08c7a
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-03-20 13:54:30 +1100

    feat(nvim): hide lsp progress messages

--- nvim/lua/ergou/plugins/ui.lua
+++ nvim/lua/ergou/plugins/ui.lua
@@ -20,6 +20,7 @@ return {
     event = 'VeryLazy',
     opts = {
       lsp = {
+        progress = { enabled = false },
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
