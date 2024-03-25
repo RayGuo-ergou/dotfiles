8ff9b2f3ecab24e6de746b46e1f450f609e0a386
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-03-26 00:07:37 +1100

    feat(nvim): add tsconfig.json and jsconfig.json to json schema store

--- nvim/lua/ergou/util/lsp.lua
+++ nvim/lua/ergou/util/lsp.lua
@@ -163,6 +163,14 @@ M.get_servers = function()
               fileMatch = { 'package.json' },
               url = 'https://json.schemastore.org/package.json',
             },
+            {
+              fileMatch = { 'tsconfig*.json' },
+              url = 'https://json.schemastore.org/tsconfig.json',
+            },
+            {
+              fileMatch = { 'jsconfig*.json' },
+              url = 'https://json.schemastore.org/jsconfig.json',
+            },
           },
         },
       },
