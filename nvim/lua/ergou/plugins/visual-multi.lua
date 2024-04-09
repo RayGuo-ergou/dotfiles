7e55ff486978940975f193e8e52ada5035121315
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-04-10 09:47:37 +1000

    chore(nvim): disable visual-multi find under mapping

--- nvim/lua/ergou/plugins/visual-multi.lua
+++ nvim/lua/ergou/plugins/visual-multi.lua
@@ -3,9 +3,9 @@ return {
   event = 'LazyFile',
   init = function()
     vim.g.VM_default_mappings = 0
-    -- vim.g.VM_maps = {
-    --   ['Find Under'] = '<C-d>',
-    -- }
+    vim.g.VM_maps = {
+      ['Find Under'] = false,
+    }
     vim.g.VM_theme = 'codedark'
   end,
 }
