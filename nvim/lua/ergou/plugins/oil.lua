010216e7dc7a9e613c96fbd66795a6531c7d0b60
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-04-10 22:33:46 +1000

    feat(nvim): add oil.nvim plugin

---
+++ nvim/lua/ergou/plugins/oil.lua
@@ -0,0 +1,32 @@
+return {
+  'stevearc/oil.nvim',
+  opts = {
+    delete_to_trash = true,
+    columns = {
+      'icon',
+      'permissions',
+      'size',
+      'mtime',
+    },
+    use_default_keymaps = false,
+    keymaps = {
+      ['g?'] = 'actions.show_help',
+      ['<CR>'] = 'actions.select',
+      ['<C-p>'] = 'actions.preview',
+      ['q'] = 'actions.close',
+      ['<backspace>'] = 'actions.parent',
+      ['_'] = 'actions.open_cwd',
+      ['gs'] = 'actions.change_sort',
+      ['H'] = 'actions.toggle_hidden',
+      ['g\\'] = 'actions.toggle_trash',
+    },
+  },
+  cmd = { 'Oil' },
+  keys = {
+    {
+      '-',
+      '<cmd>Oil<CR>',
+      desc = 'Open Oil',
+    },
+  },
+}
