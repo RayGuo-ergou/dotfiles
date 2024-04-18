1df22de402b0aa1b45836535d257fd8296615e79
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-04-18 11:53:17 +1000

    feat(zsh): add pnpm completion

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,6 +1,3 @@
-source $HOME/.env.sh
-source $DOTFILES/zsh/utils.zsh
-
 # Nix
 if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
 
@@ -41,3 +38,36 @@ unset __conda_setup
 if [[ ! -d ~/.tmux/plugins/tpm ]]; then
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins
 fi
+
+###-begin-pnpm-completion-###
+if type complete &>/dev/null; then
+  _pnpm_completion () {
+    local words cword
+    if type _get_comp_words_by_ref &>/dev/null; then
+      _get_comp_words_by_ref -n = -n @ -n : -w words -i cword
+    else
+      cword="$COMP_CWORD"
+      words=("${COMP_WORDS[@]}")
+    fi
+
+    local si="$IFS"
+    IFS=$'\n' COMPREPLY=($(COMP_CWORD="$cword" \
+                           COMP_LINE="$COMP_LINE" \
+                           COMP_POINT="$COMP_POINT" \
+                           SHELL=bash \
+                           pnpm completion-server -- "${words[@]}" \
+                           2>/dev/null)) || return $?
+    IFS="$si"
+
+    if [ "$COMPREPLY" = "__tabtab_complete_files__" ]; then
+      COMPREPLY=($(compgen -f -- "$cword"))
+    fi
+
+    if type __ltrim_colon_completions &>/dev/null; then
+      __ltrim_colon_completions "${words[cword]}"
+    fi
+  }
+  complete -o default -F _pnpm_completion pnpm
+fi
+###-end-pnpm-completion-###
+
