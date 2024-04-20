2208462f9b907c39ad5ba383e8cee84e97943f96
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-04-20 11:57:18 +1000

    feat(wezterm): disable bold

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,11 +7,37 @@ config.keys = {
   { key = 'v', mods = 'CTRL', action = act.PasteFrom('Clipboard') },
 }
 
+local font = wezterm.font({ family = 'FiraCode Nerd Font', weight = 450, stretch = 'Normal' })
+
+config.font = font
 config.font_size = 14
-config.font = wezterm.font_with_fallback({
-  { family = 'FiraCode Nerd Font', weight = 450 },
-  { family = 'JetBrains Mono' },
-})
+config.font_rules = {
+  {
+    intensity = 'Bold',
+    italic = false,
+    font = font,
+  },
+  {
+    intensity = 'Bold',
+    italic = true,
+    font = font,
+  },
+  {
+    italic = true,
+    intensity = 'Half',
+    font = font,
+  },
+  {
+    italic = false,
+    intensity = 'Half',
+    font = font,
+  },
+  {
+    italic = true,
+    intensity = 'Normal',
+    font = font,
+  },
+}
 
 config.color_scheme = 'Catppuccin Macchiato'
 
