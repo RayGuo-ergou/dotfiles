4ca4ef6514a1d38d4e9410458a93ea8f4fcd5faa
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-04-20 15:45:00 +1000

    chore: add install for git delta

--- scripts/nix/install.sh
+++ scripts/nix/install.sh
@@ -7,6 +7,7 @@ nix-env -iA nixpkgs.eza
 nix-env -iA nixpkgs.zoxide
 nix-env -iA nixpkgs.tmux
 nix-env -iA nixpkgs.git-extras
+nix-env -iA nixpkgs.delta
 nix-env -iA nixpkgs.gh
 # Adapt install gh normally do
 # sudo ln -s $HOME/.nix-profile/bin/gh /usr/bin/gh
