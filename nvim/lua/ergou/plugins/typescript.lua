32b734921bfe9b654bcb926ba92aab4c7702b01f
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-04-24 15:54:46 +1000

    feat(nvim): add support for vue-tsc in typescript plugin

--- nvim/lua/ergou/plugins/typescript.lua
+++ nvim/lua/ergou/plugins/typescript.lua
@@ -1,5 +1,25 @@
+local find_tsc_bin = function()
+  local node_modules_vue_tsc_binary = vim.fn.findfile('node_modules/.bin/vue-tsc', '.;')
+  local node_modules_tsc_binary = vim.fn.findfile('node_modules/.bin/tsc', '.;')
+
+  if node_modules_vue_tsc_binary ~= '' then
+    return node_modules_vue_tsc_binary
+  end
+
+  if node_modules_tsc_binary ~= '' then
+    return node_modules_tsc_binary
+  end
+
+  return 'tsc'
+end
 return {
   -- NOTE: add tsx filetype if doing react
   { 'dmmulroy/ts-error-translator.nvim', opts = {}, ft = { 'typescript', 'vue' } },
-  { 'dmmulroy/tsc.nvim', opts = {}, cmd = { 'TSC' } },
+  {
+    'dmmulroy/tsc.nvim',
+    opts = {
+      bin_path = find_tsc_bin(),
+    },
+    cmd = { 'TSC' },
+  },
 }
