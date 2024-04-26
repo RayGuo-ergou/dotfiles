5cb6b9ba85fcc5a8b301fd2291127d603bd9c5fa
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-04-26 16:45:08 +1000

    feat(nvim): update harpoon key

--- nvim/lua/ergou/plugins/harpoon.lua
+++ nvim/lua/ergou/plugins/harpoon.lua
@@ -3,32 +3,57 @@ return {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
     event = 'LazyFile',
-    config = function()
-      local harpoon = require('harpoon')
+    opts = {
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+      },
+      settings = {
+        save_on_toggle = true,
+      },
+    },
+    keys = function()
+      local keys = {
+        {
+          '<leader>ha',
+          function()
+            require('harpoon'):list():add()
+          end,
+          desc = 'Harpoon File',
+        },
+        {
+          '<C-e>',
+          function()
+            local harpoon = require('harpoon')
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = 'Harpoon Quick Menu',
+        },
+        {
+          '<leader>hc',
+          function()
+            require('harpoon'):list():clear()
+          end,
+          desc = 'Harpoon Clear List',
+        },
+        {
+          '<leader>hd',
+          function()
+            require('harpoon'):list():remove()
+          end,
+          desc = 'Harpoon Remove File',
+        },
+      }
 
-      local nmap = function(key, cmd, desc)
-        vim.keymap.set('n', key, cmd, { noremap = true, silent = true, desc = desc })
+      for i = 1, 5 do
+        table.insert(keys, {
+          '<leader>h' .. i,
+          function()
+            require('harpoon'):list():select(i)
+          end,
+          desc = 'Harpoon to File ' .. i,
+        })
       end
-
-      nmap('<leader>ha', function()
-        harpoon:list():add()
-      end, 'Add current file to harpoon')
-
-      nmap('<leader>hc', function()
-        harpoon:list():clear()
-      end, 'Clear harpoon list')
-
-      nmap('<leader>hd', function()
-        harpoon:list():remove()
-      end, 'Remove current file from harpoon')
-
-      nmap('<C-e>', function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end, 'Toggle harpoon quick menu')
-
-      nmap('<leader>fh', function()
-        require('ergou.util.telescope').harpoon(harpoon:list())
-      end, 'Open harpoon window with telescope')
+      return keys
     end,
   },
 }
