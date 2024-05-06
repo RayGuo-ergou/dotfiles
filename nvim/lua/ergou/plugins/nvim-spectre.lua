822f5882834f67d411a4292418a961da1db8dea6
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-05-06 23:00:12 +1000

    feat(nvim): config spectre plugin

--- nvim/lua/ergou/plugins/nvim-spectre.lua
+++ nvim/lua/ergou/plugins/nvim-spectre.lua
@@ -2,7 +2,21 @@ return {
   'nvim-pack/nvim-spectre',
   build = false,
   cmd = 'Spectre',
-  opts = { open_cmd = 'noswapfile vnew' },
+  opts = {
+    open_cmd = 'noswapfile vnew',
+    mapping = {
+      ['resume_last_search'] = {
+        map = '<leader>rs',
+        cmd = '<cmd>lua require(\'spectre\').resume_last_search()<CR>',
+        desc = 'repeat last search',
+      },
+    },
+    default = {
+      replace = {
+        cmd = 'oxi',
+      },
+    },
+  },
   keys = {
     {
       '<leader>sR',
