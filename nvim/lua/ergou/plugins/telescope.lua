b6de41192dfb63a028a2dec3f3ea98c9c55a9ba2
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-05-09 22:37:05 +1000

    feat: change telescope theme for find in current file to ivy

--- nvim/lua/ergou/plugins/telescope.lua
+++ nvim/lua/ergou/plugins/telescope.lua
@@ -43,7 +43,7 @@ return {
       '<leader>/',
       function()
         -- You can pass additional configuration to telescope to change theme, layout, etc.
-        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
+        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_ivy({
           winblend = 10,
           previewer = false,
         }))
