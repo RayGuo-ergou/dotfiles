893ae2843e48205adfd6ddd1b5540b05953e2d86
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-05-13 23:35:10 +1000

    feat: add arrow.nvim plugin

---
+++ nvim/lua/ergou/plugins/arrow.lua
@@ -0,0 +1,9 @@
+return {
+  'otavioschwanck/arrow.nvim',
+  event = 'LazyFile',
+  opts = {
+    show_icons = true,
+    leader_key = '<leader>mf',
+    buffer_leader_key = 'm', -- Per Buffer Mappings
+  },
+}
