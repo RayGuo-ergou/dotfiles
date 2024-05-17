4af39ae7642e40a47f67e3060b189376dc519de3
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-05-17 22:28:12 +1000

    refactor(nvim): plugins init after config
    This will make sure options will correctly apply to plugins.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,1 @@
--- TODO: Refactor config and lazy init.lua to make Ergou global
--- _G.Ergou = require('ergou.util')
--- bootstrap lazy.nvim, LazyVim and your plugins
 require('ergou.config')
-require('ergou.lazy')
--- nvim/lua/ergou/config/init.lua
+++ nvim/lua/ergou/config/init.lua
@@ -1,3 +1,4 @@
 require('ergou.config.options')
+require('ergou.config.lazy')
 require('ergou.config.keymaps')
 require('ergou.config.autocmds')
