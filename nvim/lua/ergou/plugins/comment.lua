f802572b8d70ed4619ee86960835f7e18d657172
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-05-21 11:38:33 +1000

    feat: inherit php comment for blade

--- nvim/lua/ergou/plugins/comment.lua
+++ nvim/lua/ergou/plugins/comment.lua
@@ -16,7 +16,7 @@ return {
   config = function(_, opts)
     local ft = require('Comment.ft')
     -- blade uses php comment for none html part
-    ft.set('blade', { '//%s', '/*%s*/' })
+    ft.set('blade', ft.get('php') or { '//%s', '/*%s*/' })
 
     require('Comment').setup(opts)
   end,
