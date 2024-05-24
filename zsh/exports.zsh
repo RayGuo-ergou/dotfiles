fe4e21147ed071a2a555ac45337fb1567bcb1a03
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-05-24 11:32:19 +1000

    feat: default use eslint_d flat

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,31 +1,31 @@
 # ZSH
 export ZSH="$HOME/.oh-my-zsh"
 
-plugins=(
-    git
-    git-extras
-    aliases
-    docker
-    ubuntu
-    bun
-    tmux
-    zsh-autosuggestions
-    zsh-syntax-highlighting
-    zsh-vi-mode
-    you-should-use
-    colored-man-pages
-    command-not-found
-    composer
-    ssh
-    copyfile
-    copypath
-    cp
-    gh
-    fd
-    ripgrep
+export plugins=(
+	git
+	git-extras
+	aliases
+	docker
+	ubuntu
+	bun
+	tmux
+	zsh-autosuggestions
+	zsh-syntax-highlighting
+	zsh-vi-mode
+	you-should-use
+	colored-man-pages
+	command-not-found
+	composer
+	ssh
+	copyfile
+	copypath
+	cp
+	gh
+	fd
+	ripgrep
 )
 
-ZSH_THEME="powerlevel10k/powerlevel10k"
+export ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # deno
 export DENO_INSTALL="$HOME/.deno"
@@ -50,8 +50,11 @@ export NVM_DIR="$HOME/.nvm"
 
 # tmux
 # Make sure the tmux omz plugin is installed
-ZSH_TMUX_AUTOSTART=true
-ZSH_TMUX_DEFAULT_SESSION_NAME='森羅万象'
+export ZSH_TMUX_AUTOSTART=true
+export ZSH_TMUX_DEFAULT_SESSION_NAME='森羅万象'
 
 # To tell pnpm not to check packageManager
 export COREPACK_ROOT="E"
+
+# eslint_d default to use flat config
+export ESLINT_USE_FLAT_CONFIG=true
