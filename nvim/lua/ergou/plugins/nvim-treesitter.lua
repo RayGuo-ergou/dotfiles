032a367ecfd2f563be4c4800687ef30f6a5dee0e
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-05-28 16:09:18 +1000

    feat: not lazy load nvim-ts-autotag

--- nvim/lua/ergou/plugins/nvim-treesitter.lua
+++ nvim/lua/ergou/plugins/nvim-treesitter.lua
@@ -280,7 +280,6 @@ return {
   -- Automatically add closing tags for HTML and JSX
   {
     'windwp/nvim-ts-autotag',
-    event = 'LazyFile',
     opts = {},
   },
   -- { 'gbprod/php-enhanced-treesitter.nvim', ft = 'php' },
