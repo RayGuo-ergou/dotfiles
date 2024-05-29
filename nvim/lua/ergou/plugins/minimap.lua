b63813073947f9b6b96c02320eff2e517405dda8
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-05-29 16:54:03 +1000

    feat(nvim): add minimap plugin

---
+++ nvim/lua/ergou/plugins/minimap.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    'gorbit99/codewindow.nvim',
+    keys = {
+      {
+        '<leader>tm',
+        function()
+          require('codewindow').toggle_minimap()
+        end,
+        desc = 'Toggle minimap',
+      },
+    },
+    event = 'LazyFile',
+    opts = {
+      auto_enable = false,
+      minimap_width = 20,
+      width_multiplier = 2,
+      screen_bounds = 'background',
+      window_border = 'none',
+      relative = 'win',
+      exclude_filetypes = {
+        'dashboard',
+        'neo-tree',
+        'lazy',
+        'lazyterm',
+        'mason',
+        'help',
+        'checkhealth',
+        'lspinfo',
+        'noice',
+        'Trouble',
+        'fish',
+        'zsh',
+      },
+    },
+  },
+}
