5a63c3741f8390a8d9d61e5e32f3a2aaefd50858
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-06-15 21:16:44 +1000

    feat: if filetype is not vue, do not reset the cache

--- nvim/lua/ergou/plugins/nvim-cmp.lua
+++ nvim/lua/ergou/plugins/nvim-cmp.lua
@@ -118,6 +118,10 @@ return {
 
     -- Reset the cache at the start of each completion session
     cmp.event:on('menu_closed', function()
+      local filetype = vim.bo.filetype
+      if filetype ~= 'vue' then
+        return
+      end
       local bufnr = vim.api.nvim_get_current_buf()
       vim.b[bufnr]._vue_ts_cached_is_in_start_tag = nil
     end)
