a8e2d9ddef2a00eb4c86c9928629597850832815
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-06-15 23:40:09 +1000

    feat: lazyload nvim-cmp plugin

--- nvim/lua/ergou/plugins/nvim-cmp.lua
+++ nvim/lua/ergou/plugins/nvim-cmp.lua
@@ -1,185 +1,187 @@
 return {
-  'hrsh7th/nvim-cmp',
-  event = 'LazyFile',
-  dependencies = {
-    'hrsh7th/cmp-buffer', -- source for text in buffer
-    'hrsh7th/cmp-path', -- source for file system paths
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/cmp-calc',
-    'L3MON4D3/LuaSnip', -- snippet engine
-    'saadparwaiz1/cmp_luasnip', -- for autocompletion
-    'rafamadriz/friendly-snippets', -- useful snippets
-    'onsails/lspkind.nvim', -- vs-code like pictograms
-    'hrsh7th/cmp-nvim-lsp-document-symbol', -- source for document symbols
-    {
-      'David-Kunz/cmp-npm',
-      event = { 'BufRead package.json' },
-      config = true,
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'LazyFile',
+    dependencies = {
+      'hrsh7th/cmp-buffer', -- source for text in buffer
+      'hrsh7th/cmp-path', -- source for file system paths
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-calc',
+      'L3MON4D3/LuaSnip', -- snippet engine
+      'saadparwaiz1/cmp_luasnip', -- for autocompletion
+      'rafamadriz/friendly-snippets', -- useful snippets
+      'onsails/lspkind.nvim', -- vs-code like pictograms
+      'hrsh7th/cmp-nvim-lsp-document-symbol', -- source for document symbols
     },
-  },
-  config = function()
-    local auto_brackets_fts = { 'lua' }
+    config = function()
+      local auto_brackets_fts = { 'lua' }
 
-    local cmp = require('cmp')
+      local cmp = require('cmp')
 
-    local luasnip = require('luasnip')
+      local luasnip = require('luasnip')
 
-    local cmp_select_next_item = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-      else
-        fallback()
+      local cmp_select_next_item = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        else
+          fallback()
+        end
       end
-    end
 
-    local cmp_select_prev_item = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-      else
-        fallback()
+      local cmp_select_prev_item = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+        else
+          fallback()
+        end
       end
-    end
 
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-    require('luasnip.loaders.from_vscode').lazy_load()
-    require('ergou.util.snips').setupSnips()
+      -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+      require('luasnip.loaders.from_vscode').lazy_load()
+      require('ergou.util.snips').setupSnips()
 
-    cmp.setup({
-      snippet = { -- configure how nvim-cmp interacts with snippet engine
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ['<C-k>'] = cmp.mapping(cmp_select_prev_item),
-        ['<C-j>'] = cmp.mapping(cmp_select_next_item),
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(), -- show completion suggestions
-        ['<C-e>'] = cmp.mapping.abort(), -- close completion window
-        ['<CR>'] = function(fallback)
-          -- Test the below later
-          -- if cmp.core.view:visible() or vim.fn.pumvisible() == 1 then
-          if cmp.visible() then
-            ergou.create_undo()
-            if cmp.confirm({ select = false }) then
-              return
+      cmp.setup({
+        snippet = { -- configure how nvim-cmp interacts with snippet engine
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-k>'] = cmp.mapping(cmp_select_prev_item),
+          ['<C-j>'] = cmp.mapping(cmp_select_next_item),
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(), -- show completion suggestions
+          ['<C-e>'] = cmp.mapping.abort(), -- close completion window
+          ['<CR>'] = function(fallback)
+            -- Test the below later
+            -- if cmp.core.view:visible() or vim.fn.pumvisible() == 1 then
+            if cmp.visible() then
+              ergou.create_undo()
+              if cmp.confirm({ select = false }) then
+                return
+              end
             end
-          end
-          return fallback()
-        end,
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          if require('copilot.suggestion').is_visible() then
-            require('copilot.suggestion').accept()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            cmp_select_next_item(fallback)
-          end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(cmp_select_prev_item),
-        ['<C-p>'] = function(fallback)
-          return fallback()
-        end,
-        ['<C-n>'] = function(fallback)
-          return fallback()
-        end,
-      }),
-      -- sources for autocompletion
-      sources = cmp.config.sources({
-        { name = 'npm', keyword_length = 4 },
-        {
-          name = 'nvim_lsp',
-          entry_filter = ergou.cmp.cmp_lsp_entry_filter,
-        },
-        { name = 'path' }, -- file system paths
-        { name = 'luasnip' }, -- snippets
-        { name = 'buffer' }, -- text within current buffer
-        { name = 'calc' }, -- simple calculator
-        {
-          name = 'lazydev',
-          group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-        },
-      }),
-      -- configure lspkind for vs-code like pictograms in completion menu
-      formatting = {
-        -- Keep the default formatting fields and expandable_indicator
-        fields = { 'abbr', 'kind', 'menu' },
-        expandable_indicator = true,
-        format = ergou.cmp.cmp_format,
-      },
-      sorting = ergou.cmp.cmp_sort(),
-    })
-    cmp.event:on('confirm_done', function(event)
-      if vim.tbl_contains(auto_brackets_fts, vim.bo.filetype) then
-        ergou.cmp.auto_brackets(event.entry)
-      end
-    end)
-
-    -- seems I already got the documentation for snippets
-    -- cmp.event:on('menu_opened', function(event)
-    --   ergou.cmp.add_missing_snippet_docs(event.window)
-    -- end)
-
-    -- Reset the cache at the start of each completion session
-    cmp.event:on('menu_closed', function()
-      local filetype = vim.bo.filetype
-      if filetype ~= 'vue' then
-        return
-      end
-      local bufnr = vim.api.nvim_get_current_buf()
-      vim.b[bufnr]._vue_ts_cached_is_in_start_tag = nil
-    end)
-
-    cmp.setup.filetype(ergou.sql_ft, {
-      sources = {
-        { name = 'vim-dadbod-completion' },
-      },
-    })
-
-    local cmd_kepmap = {
-      ['<C-j>'] = {
-        c = function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          else
-            fallback()
-          end
-        end,
-      },
-      ['<C-k>'] = {
-        c = function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          else
-            fallback()
-          end
-        end,
-      },
-    }
-    -- `/` cmdline setup.
-    cmp.setup.cmdline('/', {
-      mapping = cmp.mapping.preset.cmdline(cmd_kepmap),
-      sources = cmp.config.sources({
-        { name = 'nvim_lsp_document_symbol' },
-      }, {
-        { name = 'buffer' },
-      }),
-    })
-    -- `:` cmdline setup.
-    cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(cmd_kepmap),
-      sources = cmp.config.sources({
-        { name = 'path' },
-      }, {
-        {
-          name = 'cmdline',
-          option = {
-            ignore_cmds = { 'Man', '!' },
+            return fallback()
+          end,
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if require('copilot.suggestion').is_visible() then
+              require('copilot.suggestion').accept()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              cmp_select_next_item(fallback)
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(cmp_select_prev_item),
+          ['<C-p>'] = function(fallback)
+            return fallback()
+          end,
+          ['<C-n>'] = function(fallback)
+            return fallback()
+          end,
+        }),
+        -- sources for autocompletion
+        sources = cmp.config.sources({
+          { name = 'npm', keyword_length = 4 },
+          {
+            name = 'nvim_lsp',
+            entry_filter = ergou.cmp.cmp_lsp_entry_filter,
           },
+          { name = 'path' }, -- file system paths
+          { name = 'luasnip' }, -- snippets
+          { name = 'buffer' }, -- text within current buffer
+          { name = 'calc' }, -- simple calculator
+          {
+            name = 'lazydev',
+            group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+          },
+        }),
+        -- configure lspkind for vs-code like pictograms in completion menu
+        formatting = {
+          -- Keep the default formatting fields and expandable_indicator
+          fields = { 'abbr', 'kind', 'menu' },
+          expandable_indicator = true,
+          format = ergou.cmp.cmp_format,
         },
-      }),
-    })
-  end,
+        sorting = ergou.cmp.cmp_sort(),
+      })
+      cmp.event:on('confirm_done', function(event)
+        if vim.tbl_contains(auto_brackets_fts, vim.bo.filetype) then
+          ergou.cmp.auto_brackets(event.entry)
+        end
+      end)
+
+      -- seems I already got the documentation for snippets
+      -- cmp.event:on('menu_opened', function(event)
+      --   ergou.cmp.add_missing_snippet_docs(event.window)
+      -- end)
+
+      -- Reset the cache at the start of each completion session
+      cmp.event:on('menu_closed', function()
+        local filetype = vim.bo.filetype
+        if filetype ~= 'vue' then
+          return
+        end
+        local bufnr = vim.api.nvim_get_current_buf()
+        vim.b[bufnr]._vue_ts_cached_is_in_start_tag = nil
+      end)
+
+      cmp.setup.filetype(ergou.sql_ft, {
+        sources = {
+          { name = 'vim-dadbod-completion' },
+        },
+      })
+
+      local cmd_kepmap = {
+        ['<C-j>'] = {
+          c = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end,
+        },
+        ['<C-k>'] = {
+          c = function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end,
+        },
+      }
+      -- `/` cmdline setup.
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(cmd_kepmap),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp_document_symbol' },
+        }, {
+          { name = 'buffer' },
+        }),
+      })
+      -- `:` cmdline setup.
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(cmd_kepmap),
+        sources = cmp.config.sources({
+          { name = 'path' },
+        }, {
+          {
+            name = 'cmdline',
+            option = {
+              ignore_cmds = { 'Man', '!' },
+            },
+          },
+        }),
+      })
+    end,
+  },
+  {
+    'David-Kunz/cmp-npm',
+    event = { 'BufRead package.json' },
+    config = true,
+  },
 }
