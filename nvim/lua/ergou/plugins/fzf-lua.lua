a0dd304c0ce76542e64e4695a5ec2ff32b8b7774
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-06-16 15:12:00 +1000

    feat: enable fzf-lua and lazy load with cmd for now

--- nvim/lua/ergou/plugins/fzf-lua.lua
+++ nvim/lua/ergou/plugins/fzf-lua.lua
@@ -1,7 +1,8 @@
 return {
   -- TODO: This plugins seems pretty nice
   -- Dig more late and see if can replace telescope
-  enabled = false,
+  -- enabled = false,
   'ibhagwan/fzf-lua',
   config = true,
+  cmd = { 'FzfLua' },
 }
