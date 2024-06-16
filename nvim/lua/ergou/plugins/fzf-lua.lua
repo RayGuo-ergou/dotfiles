52f14d14a1d2154d524d12a38d204a4e5968edbc
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-06-16 20:27:53 +1000

    feat: use fzf lua for lsp symbols

--- nvim/lua/ergou/plugins/fzf-lua.lua
+++ nvim/lua/ergou/plugins/fzf-lua.lua
@@ -1,8 +1,25 @@
 return {
-  -- TODO: This plugins seems pretty nice
-  -- Dig more late and see if can replace telescope
-  -- enabled = false,
   'ibhagwan/fzf-lua',
   config = true,
   cmd = { 'FzfLua' },
+  keys = {
+    {
+      '<C-e>',
+      '<cmd>FzfLua<cr>',
+      mode = { 'n' },
+      desc = 'Open fzf-lua',
+    },
+    {
+      '<leader>ss',
+      '<cmd>FzfLua lsp_document_symbols<cr>',
+      mode = { 'n' },
+      desc = 'Find document symbols',
+    },
+    {
+      '<leader>sS',
+      '<cmd>FzfLua lsp_workspace_symbols<cr>',
+      mode = { 'n' },
+      desc = 'Find workspace symbols',
+    },
+  },
 }
