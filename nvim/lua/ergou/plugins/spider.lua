d7994c60193781f14a6767882856105841e5af36
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-06-16 21:57:10 +1000

    feat: use spider for w, b and e motions
    I found skipInsignificantPunctuation option which will do what i want

--- nvim/lua/ergou/plugins/spider.lua
+++ nvim/lua/ergou/plugins/spider.lua
@@ -1,6 +1,11 @@
 return {
   'chrisgrieser/nvim-spider',
   keys = {
-    { 'gw', '<cmd>lua require(\'spider\').motion(\'w\')<CR>', mode = { 'n', 'o', 'x' } },
+    { 'w', '<cmd>lua require(\'spider\').motion(\'w\')<CR>', mode = { 'n', 'o', 'x' } },
+    { 'b', '<cmd>lua require(\'spider\').motion(\'b\')<CR>', mode = { 'n', 'o', 'x' } },
+    { 'e', '<cmd>lua require(\'spider\').motion(\'e\')<CR>', mode = { 'n', 'o', 'x' } },
+  },
+  opts = {
+    skipInsignificantPunctuation = false,
   },
 }
