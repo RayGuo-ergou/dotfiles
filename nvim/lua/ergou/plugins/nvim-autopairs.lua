84cafe3bfcaa7da9bfedabca903e6d68f7feb90d
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-06-23 22:34:24 +1000

    feat: add <> to autopairs

--- nvim/lua/ergou/plugins/nvim-autopairs.lua
+++ nvim/lua/ergou/plugins/nvim-autopairs.lua
@@ -2,4 +2,17 @@ return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   opts = {}, -- this is equalent to setup({}) function
+  config = function(_, opts)
+    local npairs = require('nvim-autopairs')
+    local basic = require('nvim-autopairs.rules.basic')
+
+    npairs.setup(opts)
+    local config = vim.tbl_deep_extend('force', npairs.config, opts or {})
+
+    local bracket = basic.bracket_creator(config)
+
+    npairs.add_rules({
+      bracket('<', '>'),
+    })
+  end,
 }
