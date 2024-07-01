7d7f34efdf5ed6cf25d6fed820b14ea470f4fb2b
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-07-01 14:49:38 +1000

    feat: use native p and y

--- nvim/lua/ergou/plugins/yanky-nvim.lua
+++ nvim/lua/ergou/plugins/yanky-nvim.lua
@@ -44,18 +44,19 @@ return {
       desc = 'Open Yank History',
       mode = { 'i' },
     },
-    {
-      'y',
-      '<Plug>(YankyYank)',
-      mode = { 'n', 'x' },
-      desc = 'Yank text',
-    },
-    {
-      'p',
-      '<Plug>(YankyPutAfter)',
-      mode = { 'n', 'x' },
-      desc = 'Put yanked text after cursor',
-    },
+    -- Disable for speed, native y and p are faster
+    -- {
+    --   'y',
+    --   '<Plug>(YankyYank)',
+    --   mode = { 'n', 'x' },
+    --   desc = 'Yank text',
+    -- },
+    -- {
+    --   'p',
+    --   '<Plug>(YankyPutAfter)',
+    --   mode = { 'n', 'x' },
+    --   desc = 'Put yanked text after cursor',
+    -- },
     {
       'P',
       '<Plug>(YankyPutBefore)',
