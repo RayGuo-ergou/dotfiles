c5f84a08f0a688229fa6386ad79767b1d7def4ff
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-07-08 10:01:27 +1000

    feat: use vtsls as default

--- nvim/lua/ergou/config/autocmds.lua
+++ nvim/lua/ergou/config/autocmds.lua
@@ -74,6 +74,7 @@ vim.api.nvim_create_autocmd('FileType', {
     'neotest-output-panel',
     'grug-far',
     'dbout',
+    'DressingInput',
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
