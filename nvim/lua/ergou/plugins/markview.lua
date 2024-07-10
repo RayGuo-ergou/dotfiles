28b802cfd319d8234a05b9841fcd3981f244a17c
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-07-10 23:50:10 +1000

    feat: add toggle markview plugin key bind

--- nvim/lua/ergou/plugins/markview.lua
+++ nvim/lua/ergou/plugins/markview.lua
@@ -1,4 +1,11 @@
 return {
   'OXY2DEV/markview.nvim',
   ft = 'markdown',
+  keys = {
+    {
+      '<leader>mt',
+      '<cmd>Markview toggleAll<cr>',
+      desc = 'Toggle markview plugin',
+    },
+  },
 }
