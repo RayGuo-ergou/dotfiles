ac0e86f91395b404b396ebfddbf53e09ef59f16b
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-07-18 13:51:25 +1000

    feat(lualine): swap position of diff and diagnostics

--- nvim/lua/ergou/plugins/lualine.lua
+++ nvim/lua/ergou/plugins/lualine.lua
@@ -16,24 +16,7 @@ return {
           lualine_a = { 'mode' },
           lualine_b = {
             'branch',
-            {
-              'diff',
-              symbols = {
-                added = icons.git.added,
-                modified = icons.git.modified,
-                removed = icons.git.removed,
-              },
-              source = function()
-                local gitsigns = vim.b.gitsigns_status_dict
-                if gitsigns then
-                  return {
-                    added = gitsigns.added,
-                    modified = gitsigns.changed,
-                    removed = gitsigns.removed,
-                  }
-                end
-              end,
-            },
+            'diagnostics',
             {
               'windows',
             },
@@ -107,7 +90,24 @@ return {
                 return package.loaded['copilot.suggestion']
               end,
             },
-            'diagnostics',
+            {
+              'diff',
+              symbols = {
+                added = icons.git.added,
+                modified = icons.git.modified,
+                removed = icons.git.removed,
+              },
+              source = function()
+                local gitsigns = vim.b.gitsigns_status_dict
+                if gitsigns then
+                  return {
+                    added = gitsigns.added,
+                    modified = gitsigns.changed,
+                    removed = gitsigns.removed,
+                  }
+                end
+              end,
+            },
           },
           lualine_y = {
             { 'progress', separator = ' ', padding = { left = 1, right = 1 } },
