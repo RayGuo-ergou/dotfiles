ff537458b77615b96357508b525e9d3281b25661
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-07-18 10:31:53 +1000

    chore: add modes replacement
    This kind of plugin is nice but i don't feel i want to use them now so
    leave there as a note

--- nvim/lua/ergou/plugins/modes.lua
+++ nvim/lua/ergou/plugins/modes.lua
@@ -1,7 +1,18 @@
 return {
-  enabled = false,
-  'mvllow/modes.nvim',
-  opts = {
-    set_cursor = false,
+  {
+    enabled = false,
+    'mvllow/modes.nvim',
+    opts = {
+      set_cursor = false,
+    },
+  },
+  {
+    enabled = false,
+    'svampkorg/moody.nvim',
+    event = { 'ModeChanged', 'BufWinEnter', 'WinEnter' },
+    dependencies = {
+      'catppuccin/nvim',
+    },
+    opts = {},
   },
 }
