724d37036ce9e26876c06a3b2459e1275cccdf98
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-07-20 21:27:03 +1000

    feat: use mason to handle ensure install
    thanks my hero folke again

--- nvim/lua/ergou/plugins/lsp.lua
+++ nvim/lua/ergou/plugins/lsp.lua
@@ -6,7 +6,47 @@ return {
     'neovim/nvim-lspconfig',
     event = 'LazyFile',
     dependencies = {
-      { 'williamboman/mason.nvim', config = true },
+      ---@see lazyvim https://github.com/LazyVim/LazyVim/blob/6f91b406ddf2b298efe43f6467ca0a9103881a88/lua/lazyvim/plugins/lsp/init.lua#L259-L296
+      {
+        'williamboman/mason.nvim',
+        cmd = 'Mason',
+        keys = { { '<leader>cm', '<cmd>Mason<cr>', desc = 'Mason' } },
+        build = ':MasonUpdate',
+        opts_extend = { 'ensure_installed' },
+        opts = {
+          ensure_installed = {
+            'stylua',
+            'eslint_d',
+            'phpcbf',
+            'cspell',
+            'phpcs',
+            'prettierd',
+          },
+        },
+        ---@param opts MasonSettings | {ensure_installed: string[]}
+        config = function(_, opts)
+          require('mason').setup(opts)
+          local mr = require('mason-registry')
+          mr:on('package:install:success', function()
+            vim.defer_fn(function()
+              -- trigger FileType event to possibly load this newly installed LSP server
+              require('lazy.core.handler.event').trigger({
+                event = 'FileType',
+                buf = vim.api.nvim_get_current_buf(),
+              })
+            end, 100)
+          end)
+
+          mr.refresh(function()
+            for _, tool in ipairs(opts.ensure_installed) do
+              local p = mr.get_package(tool)
+              if not p:is_installed() then
+                p:install()
+              end
+            end
+          end)
+        end,
+      },
       { 'williamboman/mason-lspconfig.nvim', config = function() end },
       {
         'utilyre/barbecue.nvim',
@@ -80,17 +120,6 @@ return {
             require('lspconfig')[server_name].setup(server)
           end,
         },
-      })
-    end,
-  },
-  {
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-    config = function()
-      local servers = Util.lsp.get_servers()
-      local ensure_installed =
-        vim.list_extend(vim.tbl_keys(servers), { 'stylua', 'eslint_d', 'phpcbf', 'cspell', 'phpcs', 'prettierd' })
-      require('mason-tool-installer').setup({
-        ensure_installed = ensure_installed,
       })
     end,
   },
