e5ba9cff421b416fd60c692e84fcd48719818e37
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-07-21 20:21:59 +1000

    feat: change to another markdown plugin

--- nvim/lua/ergou/plugins/markview.lua
+++ nvim/lua/ergou/plugins/markview.lua
@@ -1,11 +1,18 @@
 return {
-  'OXY2DEV/markview.nvim',
-  ft = 'markdown',
-  keys = {
-    {
-      '<leader>mt',
-      '<cmd>Markview toggleAll<cr>',
-      desc = 'Toggle markview plugin',
+  {
+    enabled = false,
+    'OXY2DEV/markview.nvim',
+    ft = 'markdown',
+    keys = {
+      {
+        '<leader>mt',
+        '<cmd>Markview toggleAll<cr>',
+        desc = 'Toggle markview plugin',
+      },
     },
+  },
+  {
+    'MeanderingProgrammer/markdown.nvim',
+    opts = {},
   },
 }
