8545543b6d53f06b872c64c35d84c862c9cc9bb9
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-07-22 21:43:06 +1000

    feat: update grug far as upstream updated

--- nvim/lua/ergou/plugins/grug-far.lua
+++ nvim/lua/ergou/plugins/grug-far.lua
@@ -6,15 +6,12 @@ return {
     {
       '<leader>rr',
       function()
-        local is_visual = vim.fn.mode():lower():find('v')
-        if is_visual then -- needed to make visual selection work
-          vim.cmd([[normal! v]])
-        end
         local grug = require('grug-far')
         local ext = vim.bo.buftype == '' and vim.fn.expand('%:e')
-        local filesFilter = ext and ext ~= '' and '*.' .. ext or nil;
-        (is_visual and grug.with_visual_selection or grug.grug_far)({
-          prefills = { filesFilter = filesFilter },
+        grug.grug_far({
+          prefills = {
+            filesFilter = ext and ext ~= '' and '*.' .. ext or nil,
+          },
         })
       end,
       mode = { 'n', 'v' },
