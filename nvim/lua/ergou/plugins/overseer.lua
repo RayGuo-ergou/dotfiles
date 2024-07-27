279f89a7797792fae52b87e64c655d4a414cf359
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-07-27 20:30:22 +1000

    feat: config overseer

--- nvim/lua/ergou/plugins/overseer.lua
+++ nvim/lua/ergou/plugins/overseer.lua
@@ -1,4 +1,31 @@
 return {
   'stevearc/overseer.nvim',
   opts = {},
+  cmd = {
+    'OverseerOpen',
+    'OverseerClose',
+    'OverseerToggle',
+    'OverseerSaveBundle',
+    'OverseerLoadBundle',
+    'OverseerDeleteBundle',
+    'OverseerRunCmd',
+    'OverseerRun',
+    'OverseerInfo',
+    'OverseerBuild',
+    'OverseerQuickAction',
+    'OverseerTaskAction',
+    'OverseerClearCache',
+  },
+  keys = {
+    {
+      '<leader>to',
+      '<cmd>OverseerToggle right<cr>',
+      desc = 'Toggle Overseer',
+    },
+    {
+      '<leader>ro',
+      '<cmd>OverseerRun<cr>',
+      desc = 'Run Overseer',
+    },
+  },
 }
