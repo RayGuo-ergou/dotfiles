68fb1d82950d9155a73db5928c4da3acf590cd23
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-08-02 09:25:30 +1000

    chore: update note for yazi 0.3.0

--- system-init/archive/nix/install.sh
+++ system-init/archive/nix/install.sh
@@ -13,3 +13,8 @@ nix profile install nixpkgs#delta nixpkgs#gh nixpkgs#git-extras nixpkgs#hyfetch 
 # Remove all pkg and reinstall with flask above
 nix profile remove delta gh git-extras hyfetch lazygit tmux typos wslu xsel zoxide fzf ripgrep yazi eza bat
 
+# Yazi 0.3.0 new deps
+nix profile install nixpkgs#_7zz
+nix profile install nixpkgs#imagemagick
+nix profile install nixpkgs#chafa
+
