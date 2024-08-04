512ae5601614973d4f89c4210e9ecf6d0f2428b9
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-08-04 21:33:02 +1000

    feat: update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,18 +1,38 @@
 local wezterm = require('wezterm')
 
+local color_scheme = 'Catppuccin Macchiato'
+local color_scheme_colors = wezterm.color.get_builtin_schemes()[color_scheme]
+local home_dir = os.getenv('HOME') or os.getenv('USERPROFILE')
+
 local act = wezterm.action
 local config = {}
 config.keys = {
   -- paste from the clipboard
   { key = 'v', mods = 'CTRL', action = act.PasteFrom('Clipboard') },
 }
+config.color_scheme = color_scheme
+config.window_padding = {
+
+  bottom = 4,
+}
+config.background = {
+  {
+    source = {
+      Color = color_scheme_colors.background,
+    },
+    width = '100%',
+    height = '100%',
+  },
+  -- Change accordingly as cannot create symbol link between windows and wsl
+  -- {
+  --   source = {
+  --     File = home_dir .. '\\OneDrive\\Image\\本机照片\\20231220_212608208_iOS.jpg',
+  --   },
+  --   opacity = 0.1,
+  -- },
+}
 
 local font = wezterm.font({ family = 'FiraCode Nerd Font', weight = 450, stretch = 'Normal' })
-
-config.font = font
-config.window_decorations = 'NONE'
-config.font_size = 14
-config.hide_tab_bar_if_only_one_tab = true
 config.font_rules = {
   {
     intensity = 'Bold',
@@ -41,6 +61,9 @@ config.font_rules = {
   },
 }
 
-config.color_scheme = 'Catppuccin Macchiato'
+config.font = font
+config.window_decorations = 'RESIZE'
+config.font_size = 14
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
