e66328a0353fd247e95c52bb30b9caa0eab84017
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-08-08 09:53:43 +1000

    perf: lazy load todo comment

--- nvim/lua/ergou/plugins/todo-comments.lua
+++ nvim/lua/ergou/plugins/todo-comments.lua
@@ -2,57 +2,37 @@ return {
   'folke/todo-comments.nvim',
   cmd = { 'TodoTrouble', 'TodoTelescope' },
   event = 'LazyFile',
-  config = true,
-  keys = function()
+  config = function(_, opts)
     local ts_repeat_move = require('nvim-treesitter.textobjects.repeatable_move')
     local todo = require('todo-comments')
     local todo_next, todo_prev = ts_repeat_move.make_repeatable_move_pair(todo.jump_next, todo.jump_prev)
     local Util = require('ergou.util')
+    local map = vim.keymap.set
 
-    local picker_keys = function()
-      if Util.pick.picker.name == 'telescope' then
-        return {
-          { '<leader>st', '<cmd>TodoTelescope<cr>', desc = 'Todo' },
-          { '<leader>sT', '<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>', desc = 'Todo/Fix/Fixme' },
-        }
-      end
-      if Util.pick.picker.name == 'fzf' then
-        return {
-          {
-            '<leader>st',
-            function()
-              require('todo-comments.fzf').todo()
-            end,
-            desc = 'Todo',
-          },
-          {
-            '<leader>sT',
-            function()
-              require('todo-comments.fzf').todo({ keywords = { 'TODO', 'FIX', 'FIXME' } })
-            end,
-            desc = 'Todo/Fix/Fixme',
-          },
-        }
-      end
-
-      return {}
+    if Util.pick.picker.name == 'telescope' then
+      map('n', '<leader>st', '<cmd>TodoTelescope<cr>', { desc = 'Todo' })
+      map('n', '<leader>sT', '<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>', { desc = 'Todo/Fix/Fixme' })
     end
-
-    return vim.list_extend(picker_keys(), {
-      {
-        ']t',
-        function()
-          todo_next()
-        end,
-        desc = 'Next todo comment',
-      },
-      {
-        '[t',
-        function()
-          todo_prev()
-        end,
-        desc = 'Previous todo comment',
-      },
-    })
+    if Util.pick.picker.name == 'fzf' then
+      map('n', '<leader>st', function()
+        require('todo-comments.fzf').todo()
+      end, { desc = 'Todo' })
+      map('n', '<leader>sT', function()
+        require('todo-comments.fzf').todo({ keywords = { 'TODO', 'FIX', 'FIXME' } })
+      end, { desc = 'Todo/Fix/Fixme' })
+    end
+    map('n', ']t', function()
+      todo_next()
+    end, { desc = 'Next todo comment' })
+    map('n', '[t', function()
+      todo_prev()
+    end, { desc = 'Previous todo comment' })
+    require('todo-comments').setup(opts)
   end,
+  keys = {
+    { ']t' },
+    { '[t' },
+    { '<leader>st' },
+    { '<leader>sT' },
+  },
 }
