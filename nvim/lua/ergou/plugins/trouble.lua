7f1004cd754fc70bef2bb085366ebaff33c6c930
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-08-08 10:06:05 +1000

    chore: remove unnesscery function

--- nvim/lua/ergou/plugins/trouble.lua
+++ nvim/lua/ergou/plugins/trouble.lua
@@ -32,45 +32,43 @@ return {
 
       require('trouble').setup(opts)
     end,
-    keys = function()
-      return {
-        {
-          '<leader>xx',
-          '<cmd>Trouble diagnostics toggle<cr>',
-          desc = 'Diagnostics (Trouble)',
-        },
-        {
-          '<leader>xX',
-          '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-          desc = 'Buffer Diagnostics (Trouble)',
-        },
-        {
-          '<leader>cs',
-          '<cmd>Trouble symbols toggle focus=false<cr>',
-          desc = 'Symbols (Trouble)',
-        },
-        {
-          '<leader>cl',
-          '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-          desc = 'LSP Definitions / references / ... (Trouble)',
-        },
-        {
-          '<leader>xL',
-          '<cmd>Trouble loclist toggle<cr>',
-          desc = 'Location List (Trouble)',
-        },
-        {
-          '<leader>xQ',
-          '<cmd>Trouble qflist toggle<cr>',
-          desc = 'Quickfix List (Trouble)',
-        },
-        { '<leader>xt', '<cmd>Trouble todo toggle<cr>', desc = 'Todo (Trouble)' },
-        { '<leader>xT', '<cmd>Trouble todo toggle keywords=TODO,FIX,FIXME<cr>', desc = 'Todo/Fix/Fixme (Trouble)' },
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+      { '<leader>xt', '<cmd>Trouble todo toggle<cr>', desc = 'Todo (Trouble)' },
+      { '<leader>xT', '<cmd>Trouble todo toggle keywords=TODO,FIX,FIXME<cr>', desc = 'Todo/Fix/Fixme (Trouble)' },
 
-        { '[q' },
-        { ']q' },
-      }
-    end,
+      { '[q' },
+      { ']q' },
+    },
     opts = {}, -- for default options, refer to the configuration section for custom setup.
   },
 }
