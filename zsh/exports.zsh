0efad8730ab1e1273829699cdf07c195e17ad4af
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-08-14 19:46:52 +1000

    feat: fzf ctrl-r copy do not abort

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -94,7 +94,7 @@ export FZF_DEFAULT_OPTS="
 export FZF_CTRL_R_OPTS="
   --preview 'echo {}' --preview-window up:3:hidden:wrap
   --bind 'ctrl-/:toggle-preview'
-  --bind 'ctrl-y:execute-silent(echo -n {2..} | xsel -b)+abort'
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | xsel -b)'
   --color header:italic
   --header 'Press CTRL-Y to copy command into clipboard'"
 
