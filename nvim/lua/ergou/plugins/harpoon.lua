9214d5e028bc1f75caa4bf9367f8027dbf5ef879
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-08-19 09:42:23 +1000

    feat: change harpoon key

--- nvim/lua/ergou/plugins/harpoon.lua
+++ nvim/lua/ergou/plugins/harpoon.lua
@@ -20,7 +20,7 @@ return {
           desc = 'Harpoon File',
         },
         {
-          '<C-t>',
+          'gh',
           function()
             local harpoon = require('harpoon')
             harpoon.ui:toggle_quick_menu(harpoon:list())
