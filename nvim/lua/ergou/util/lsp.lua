2a1097a48519baa1aac080dc50d258b2dd9f8fca
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-08-29 11:23:52 +1000

    feat: ts didRename only for vtsls

--- nvim/lua/ergou/util/lsp.lua
+++ nvim/lua/ergou/util/lsp.lua
@@ -89,17 +89,20 @@ M.TYPESCRIPT.on_attach = function(client, _)
 
   existing_capabilities.documentFormattingProvider = nil
 
-  local existing_filters = existing_capabilities.workspace.fileOperations.didRename.filters or {}
-  local new_glob = '**/*.{ts,cts,mts,tsx,js,cjs,mjs,jsx,vue}'
+  if client.name == 'vtsls' then
+    local existing_filters = existing_capabilities.workspace.fileOperations.didRename.filters or {}
+    local new_glob = '**/*.{ts,cts,mts,tsx,js,cjs,mjs,jsx,vue}'
 
-  for _, filter in ipairs(existing_filters) do
-    if filter.pattern and filter.pattern.matches == 'file' then
-      filter.pattern.glob = new_glob
-      break
+    for _, filter in ipairs(existing_filters) do
+      if filter.pattern and filter.pattern.matches == 'file' then
+        filter.pattern.glob = new_glob
+        break
+      end
     end
+
+    existing_capabilities.workspace.fileOperations.didRename.filters = existing_filters
   end
 
-  existing_capabilities.workspace.fileOperations.didRename.filters = existing_filters
   client.server_capabilities = existing_capabilities
 end
 
