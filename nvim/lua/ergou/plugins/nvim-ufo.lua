b56508b24c8c05840b25241abe14f4783d8e69d1
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-09-03 09:43:49 +1000

    chore(hack): use fork of ufo until pr merged

--- nvim/lua/ergou/plugins/nvim-ufo.lua
+++ nvim/lua/ergou/plugins/nvim-ufo.lua
@@ -1,5 +1,7 @@
 return {
   'kevinhwang91/nvim-ufo',
+  url = 'https://github.com/RayGuo-ergou/nvim-ufo',
+  branch = 'feature/nvim_11_ts_breaking_change',
   dependencies = { 'kevinhwang91/promise-async' },
   event = 'LazyFile',
   init = function()
