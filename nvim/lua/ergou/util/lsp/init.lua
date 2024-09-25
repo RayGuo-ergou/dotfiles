156f54dcf4b45d4deda318612ef7a9292b9aae44
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-09-25 21:53:41 +1000

    feat: vue tsconfig json schema

--- nvim/lua/ergou/util/lsp/init.lua
+++ nvim/lua/ergou/util/lsp/init.lua
@@ -395,7 +395,16 @@ M.get_servers = function()
     jsonls = {
       settings = {
         json = {
-          schemas = require('schemastore').json.schemas(),
+          schemas = require('schemastore').json.schemas({
+            extra = {
+              {
+                description = 'TypeScript compiler configuration file for vue',
+                fileMatch = { 'tsconfig*.json' },
+                name = 'vue-tsconfig.json',
+                url = 'https://raw.githubusercontent.com/vuejs/language-tools/master/packages/language-core/schemas/vue-tsconfig.schema.json',
+              },
+            },
+          }),
           validate = { enable = true },
         },
       },
