460233b495cd8a6a5fc702204e028957bcbc2489
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-10-07 09:06:38 +1100

    chore: correct nix install path

--- system-init/archive/nix/install.sh
+++ system-init/archive/nix/install.sh
@@ -7,7 +7,7 @@ sudo ln -s $HOME/.nix-profile/bin/gh /usr/bin/gh
 # To upgrade all pkg
 nix profile upgrade --all
 
-nix profile install nixpkgs#delta nixpkgs#gh nixpkgs#git-extras nixpkgs#hyfetch nixpkgs#lazygit nixpkgs#tmux nixpkgs#typos nixpkgs#wslu nixpkgs#xsel nixpkgs#zoxide nixpkgs#fzf nixpkgs#ripgrep nixpkgs#yazi nixpkgs#eza nixpkgs#bat nixpkgs#stripe-cli nixos.gitmux
+nix profile install nixpkgs#delta nixpkgs#gh nixpkgs#git-extras nixpkgs#hyfetch nixpkgs#lazygit nixpkgs#tmux nixpkgs#typos nixpkgs#wslu nixpkgs#xsel nixpkgs#zoxide nixpkgs#fzf nixpkgs#ripgrep nixpkgs#yazi nixpkgs#eza nixpkgs#bat nixpkgs#stripe-cli nixpkgs#gitmux
 
 # For gitmux, have to link to /usr/bin just like gh cli
 sudo ln -s $HOME/.nix-profile/bin/gitmux /usr/bin/gitmux
