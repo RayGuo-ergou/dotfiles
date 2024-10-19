8143f8dede18adce8d6f8cbeefa508b902aea502
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-10-19 18:36:46 +1100

    feat: lsp_interop init

--- nvim/lua/ergou/plugins/nvim-treesitter.lua
+++ nvim/lua/ergou/plugins/nvim-treesitter.lua
@@ -171,6 +171,15 @@ return {
             ['<leader>rA'] = '@parameter.inner',
           },
         },
+        lsp_interop = {
+          enable = true,
+          border = 'none',
+          floating_preview_opts = {},
+          peek_definition_code = {
+            ['<leader>df'] = '@function.outer',
+            ['<leader>dF'] = '@class.outer',
+          },
+        },
       },
     },
     ---@param opts TSConfig
