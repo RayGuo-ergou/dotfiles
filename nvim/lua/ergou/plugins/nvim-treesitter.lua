30cb812a18e2d74e83a1d16afb047c94fd1fc117
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-10-30 11:32:58 +1100

    feat: TS context multiwindow

--- nvim/lua/ergou/plugins/nvim-treesitter.lua
+++ nvim/lua/ergou/plugins/nvim-treesitter.lua
@@ -247,7 +247,11 @@ return {
   {
     'nvim-treesitter/nvim-treesitter-context',
     event = 'LazyFile',
-    opts = { mode = 'cursor', max_lines = 3 },
+    opts = {
+      mode = 'cursor',
+      max_lines = 3,
+      multiwindow = true,
+    },
     keys = {
       {
         '<leader>ut',
