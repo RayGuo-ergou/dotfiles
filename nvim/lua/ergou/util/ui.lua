6fa2c2aff0a3d4d5bc287f288dce3563e13502f9
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-11-14 19:48:11 +1100

    feat: ui color drop deprecated api

--- nvim/lua/ergou/util/ui.lua
+++ nvim/lua/ergou/util/ui.lua
@@ -2,11 +2,8 @@
 local M = {}
 
 function M.fg(name)
-  ---@type {foreground?:number}?
-  ---@diagnostic disable-next-line: deprecated
-  local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name }) or vim.api.nvim_get_hl_by_name(name, true)
-  ---@diagnostic disable-next-line: undefined-field
-  local fg = hl and (hl.fg or hl.foreground)
+  local hl = vim.api.nvim_get_hl(0, { name = name, link = false })
+  local fg = hl and hl.fg or hl.foreground
   return fg and { fg = string.format('#%06x', fg) } or nil
 end
 
