c4f1cf876f70e846d1b9e4e885bc83c017a86cb0
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-11-25 19:49:56 +1100

    feat: use luals for uv

--- nvim/lua/ergou/plugins/lazydev.lua
+++ nvim/lua/ergou/plugins/lazydev.lua
@@ -4,15 +4,11 @@ return {
     ft = 'lua', -- only load on lua files
     opts = {
       library = {
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
         { path = 'nvim-lspconfig/lua', words = { 'lsp' } },
         { path = 'markdown.nvim/lua/render-markdown', words = { 'render%.md' } },
         { path = 'snacks.nvim', words = { 'Snacks' } },
       },
     },
   },
-  {
-    'Bilal2453/luvit-meta',
-    lazy = true,
-  }, -- optional `vim.uv` typings
 }
