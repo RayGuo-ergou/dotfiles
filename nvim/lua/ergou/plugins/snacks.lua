55f124e345f7a4ad4c77646969183b5a866a4f07
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-12-03 18:45:04 +1100

    feat: add snacks scratch keybind

--- nvim/lua/ergou/plugins/snacks.lua
+++ nvim/lua/ergou/plugins/snacks.lua
@@ -59,6 +59,20 @@ return {
   },
   keys = {
     {
+      '<leader>.',
+      function()
+        Snacks.scratch()
+      end,
+      desc = 'Toggle Scratch Buffer',
+    },
+    {
+      '<leader>S',
+      function()
+        Snacks.scratch.select()
+      end,
+      desc = 'Select Scratch Buffer',
+    },
+    {
       '<leader>sN',
       function()
         Snacks.notifier.show_history()
