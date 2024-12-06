bc40af29e4da28b95b63d91ddc0cdd593776c4be
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-12-06 15:35:36 +1100

    feat: add select session keybind

--- nvim/lua/ergou/plugins/persistence-nvim.lua
+++ nvim/lua/ergou/plugins/persistence-nvim.lua
@@ -11,6 +11,13 @@ return {
       desc = 'Restore Session',
     },
     {
+      '<leader>qS',
+      function()
+        require('persistence').select()
+      end,
+      desc = 'Select Session',
+    },
+    {
       '<leader>ql',
       function()
         require('persistence').load({ last = true })
