523391bdb976bdefb99f413ae22af19cca719134
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-12-11 21:22:33 +1100

    feat: create topope for ungrouped tpope plugins

---
+++ nvim/lua/ergou/plugins/tpope.lua
@@ -0,0 +1,8 @@
+-- This file contains ungrouped tpope plugins,
+-- e.g. dadbod has it own group which includes all dadbod related plugins
+return {
+  {
+    'tpope/vim-sleuth',
+    event = 'LazyFile',
+  },
+}
