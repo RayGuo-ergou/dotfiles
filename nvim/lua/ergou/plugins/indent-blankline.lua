a30acffff910088df6bd59841146cfefdf5c8981
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-12-11 21:19:30 +1100

    chore: remove indent-blankline

--- nvim/lua/ergou/plugins/indent-blankline.lua
+++ nvim/lua/ergou/plugins/indent-blankline.lua
@@ -4,30 +4,4 @@ return {
     'tpope/vim-sleuth',
     event = 'LazyFile',
   },
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    enabled = false,
-    event = 'LazyFile',
-    main = 'ibl',
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {
-      exclude = {
-        filetypes = {
-          'help',
-          'alpha',
-          'dashboard',
-          'neo-tree',
-          'Trouble',
-          'trouble',
-          'lazy',
-          'mason',
-          'notify',
-          'toggleterm',
-          'lazyterm',
-          'snacks_dashboard',
-        },
-      },
-    },
-  },
 }
