dc74e41cbe1999747dfb3afc1ba556a7109a80b8
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-12-11 11:57:49 +1100

    chore: remove temp fork for tsc nvim

--- nvim/lua/ergou/plugins/typescript.lua
+++ nvim/lua/ergou/plugins/typescript.lua
@@ -17,7 +17,6 @@ return {
   { 'dmmulroy/ts-error-translator.nvim', opts = {}, ft = { 'typescript', 'vue' } },
   {
     'dmmulroy/tsc.nvim',
-    url = 'https://github.com/RayGuo-ergou/tsc.nvim',
     opts = {
       bin_path = find_tsc_bin(),
       -- enable_progress_notifications = false,
