44e78b1f176cc39110e376371997e595859eb2c9
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-12-16 09:12:20 +1100

    feat: better escape for snippet

--- nvim/lua/ergou/config/keymaps.lua
+++ nvim/lua/ergou/config/keymaps.lua
@@ -136,8 +136,8 @@ ergou.toggle.spelling():map('<leader>us')
 ergou.toggle.zen():map('<leader>z')
 ergou.toggle.zoom():map('<leader>wm'):map('<leader>Z')
 
----On key events---
-Snacks.util.on_key('<esc>', function()
+-- Clear search and stop snippet on escape
+map({ 'i', 'n', 's' }, '<esc>', function()
   local luasnip = require('luasnip')
   if luasnip.expand_or_jumpable() then
     luasnip.unlink_current()
@@ -145,4 +145,5 @@ Snacks.util.on_key('<esc>', function()
   if vim.snippet then
     vim.snippet.stop()
   end
-end)
+  return '<esc>'
+end, { expr = true, desc = 'Escape and clear snippet' })
