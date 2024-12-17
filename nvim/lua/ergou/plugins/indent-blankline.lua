883733424a13c32ff8363e8eb6f9a2e542fcad35
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-12-18 10:04:18 +1100

    feat: enable indent-blankline

--- nvim/lua/ergou/plugins/indent-blankline.lua
+++ nvim/lua/ergou/plugins/indent-blankline.lua
@@ -1,32 +1,24 @@
 return {
-  -- Detect tabstop and shiftwidth automatically
-  {
-    'tpope/vim-sleuth',
-    event = 'LazyFile',
-  },
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    enabled = false,
-    event = 'LazyFile',
-    main = 'ibl',
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {
-      exclude = {
-        filetypes = {
-          'help',
-          'alpha',
-          'dashboard',
-          'neo-tree',
-          'Trouble',
-          'trouble',
-          'lazy',
-          'mason',
-          'notify',
-          'toggleterm',
-          'lazyterm',
-          'snacks_dashboard',
-        },
+  'lukas-reineke/indent-blankline.nvim',
+  event = 'LazyFile',
+  main = 'ibl',
+  ---@module "ibl"
+  ---@type ibl.config
+  opts = {
+    exclude = {
+      filetypes = {
+        'help',
+        'alpha',
+        'dashboard',
+        'neo-tree',
+        'Trouble',
+        'trouble',
+        'lazy',
+        'mason',
+        'notify',
+        'toggleterm',
+        'lazyterm',
+        'snacks_dashboard',
       },
     },
   },
