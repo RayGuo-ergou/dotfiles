3e95df9bd851a3cb7957998ba14a0a8e9f99b6e9
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-12-27 23:41:09 +1100

    feat: tmux keybind add i and x

--- nvim/lua/ergou/plugins/tmux.lua
+++ nvim/lua/ergou/plugins/tmux.lua
@@ -17,56 +17,56 @@ return {
     keys = {
       {
         '<C-j>',
-        mode = { 'n' },
+        mode = { 'n', 'i', 'x' },
         function()
           require('tmux').move_bottom()
         end,
       },
       {
         '<C-l>',
-        mode = { 'n' },
+        mode = { 'n', 'i', 'x' },
         function()
           require('tmux').move_right()
         end,
       },
       {
         '<C-h>',
-        mode = { 'n' },
+        mode = { 'n', 'i', 'x' },
         function()
           require('tmux').move_left()
         end,
       },
       {
         '<C-k>',
-        mode = { 'n' },
+        mode = { 'n', 'i', 'x' },
         function()
           require('tmux').move_top()
         end,
       },
       {
         '<C-down>',
-        mode = { 'n' },
+        mode = { 'n', 'i', 'x' },
         function()
           require('tmux').resize_bottom()
         end,
       },
       {
         '<C-up>',
-        mode = { 'n' },
+        mode = { 'n', 'i', 'x' },
         function()
           require('tmux').resize_top()
         end,
       },
       {
         '<C-right>',
-        mode = { 'n' },
+        mode = { 'n', 'i', 'x' },
         function()
           require('tmux').resize_right()
         end,
       },
       {
         '<C-left>',
-        mode = { 'n' },
+        mode = { 'n', 'i', 'x' },
         function()
           require('tmux').resize_left()
         end,
