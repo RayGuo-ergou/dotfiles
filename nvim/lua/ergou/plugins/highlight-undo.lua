83e0a5a549869e62a666c095a50d5559b6a2717f
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-02-15 14:55:01 +1100

    feat: default cb at upstream level

--- nvim/lua/ergou/plugins/highlight-undo.lua
+++ nvim/lua/ergou/plugins/highlight-undo.lua
@@ -1,21 +1,5 @@
 return {
   'tzachar/highlight-undo.nvim',
   event = 'LazyFile',
-  opts = {
-    ignore_cb = function(buf)
-      local bo = vim.bo[buf]
-
-      -- Check for special buffer types
-      if bo.buftype ~= '' then
-        return true
-      end
-
-      -- Check if buffer is modifiable
-      if not bo.modifiable then
-        return true
-      end
-
-      return false
-    end,
-  },
+  opts = {},
 }
