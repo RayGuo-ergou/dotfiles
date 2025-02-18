7d6e914e84ac2a722be2965ffd3d39785855cff9
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-02-18 16:12:18 +1100

    fix: keymap conflict

--- nvim/lua/ergou/plugins/treewalker.lua
+++ nvim/lua/ergou/plugins/treewalker.lua
@@ -11,9 +11,9 @@ return {
     { '<Right>', '<cmd>Treewalker Right<cr>', desc = 'Tree walk right' },
 
     -- swapping
-    { '<C-Up>', '<cmd>Treewalker SwapUp<cr>', desc = 'Tree swap up' },
-    { '<C-Down>', '<cmd>Treewalker SwapDown<cr>', desc = 'Tree swap down' },
-    { '<C-Left>', '<cmd>Treewalker SwapLeft<cr>', desc = 'Tree swap left' },
-    { '<C-Right>', '<cmd>Treewalker SwapRight<cr>', desc = 'Tree swap right' },
+    { '<M-Up>', '<cmd>Treewalker SwapUp<cr>', desc = 'Tree swap up' },
+    { '<M-Down>', '<cmd>Treewalker SwapDown<cr>', desc = 'Tree swap down' },
+    { '<M-Left>', '<cmd>Treewalker SwapLeft<cr>', desc = 'Tree swap left' },
+    { '<M-Right>', '<cmd>Treewalker SwapRight<cr>', desc = 'Tree swap right' },
   },
 }
