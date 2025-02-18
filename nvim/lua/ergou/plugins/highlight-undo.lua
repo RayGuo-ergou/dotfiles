d2e231ed228094bffe52ab26053789adb1d97b7f
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-02-18 11:40:05 +1100

    feat: change highlight undo plugin

--- nvim/lua/ergou/plugins/highlight-undo.lua
+++ nvim/lua/ergou/plugins/highlight-undo.lua
@@ -1,5 +1,31 @@
 return {
-  'tzachar/highlight-undo.nvim',
-  event = 'LazyFile',
-  opts = {},
+  {
+    'y3owk1n/undo-glow.nvim',
+    ---@param _ any
+    ---@param opts UndoGlow.Config
+    opts = function(_, opts)
+      local has_catppuccin, catppuccin = pcall(require, 'catppuccin.palettes')
+
+      if has_catppuccin then
+        ---@type CtpColors<string>
+        local colors = catppuccin.get_palette()
+        opts.undo_hl_color = { bg = colors.surface2 }
+        opts.redo_hl_color = { bg = colors.surface2 }
+      end
+    end,
+    ---@param _ any
+    ---@param opts UndoGlow.Config
+    config = function(_, opts)
+      local undo_glow = require('undo-glow')
+
+      undo_glow.setup(opts)
+
+      vim.keymap.set('n', 'u', undo_glow.undo, { noremap = true, silent = true })
+      vim.keymap.set('n', '<c-r>', undo_glow.redo, { noremap = true, silent = true })
+    end,
+    keys = {
+      { 'u', desc = 'Undo' },
+      { '<c-r>', desc = 'Redo' },
+    },
+  },
 }
