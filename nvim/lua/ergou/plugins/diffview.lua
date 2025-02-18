2eb0b1788561ad5de620d82dbd0baebc7144eb08
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-02-19 09:49:09 +1100

    feat: diffview key map

--- nvim/lua/ergou/plugins/diffview.lua
+++ nvim/lua/ergou/plugins/diffview.lua
@@ -1,9 +1,22 @@
 return {
   'sindrets/diffview.nvim',
-  opts = {
-    enhanced_diff_hl = true,
-    hooks = {}, -- See ':h diffview-config-hooks'
-  },
+  opts = function()
+    local actions = require('diffview.actions')
+    local scrollKeyMapping = {
+      ['<c-b>'] = false,
+      ['<c-f>'] = false,
+      { 'n', '<c-u>', actions.scroll_view(-0.25), { desc = 'Scroll the view up' } },
+      { 'n', '<c-d>', actions.scroll_view(0.25), { desc = 'Scroll the view down' } },
+    }
+    return {
+      keymaps = {
+        file_panel = scrollKeyMapping,
+        file_history_panel = scrollKeyMapping,
+      },
+      enhanced_diff_hl = true,
+      hooks = {}, -- See ':h diffview-config-hooks'
+    }
+  end,
   cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
   keys = {
     {
