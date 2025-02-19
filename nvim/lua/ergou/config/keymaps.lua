f68f215f8bd70621d868d575062caebc4c4783f6
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-02-19 11:52:17 +1100

    fix: keymap conflict

--- nvim/lua/ergou/config/keymaps.lua
+++ nvim/lua/ergou/config/keymaps.lua
@@ -47,9 +47,7 @@ map({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save file' })
 
 -- better up/down
 map({ 'n', 'x' }, 'j', 'v:count == 0 ? \'gj\' : \'j\'', { expr = true, silent = true })
-map({ 'n', 'x' }, '<Down>', 'v:count == 0 ? \'gj\' : \'j\'', { expr = true, silent = true })
 map({ 'n', 'x' }, 'k', 'v:count == 0 ? \'gk\' : \'k\'', { expr = true, silent = true })
-map({ 'n', 'x' }, '<Up>', 'v:count == 0 ? \'gk\' : \'k\'', { expr = true, silent = true })
 
 -- buffers
 map('n', '<leader>bb', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
