704d4a5e678c25b8079230f036e65e983fd0c8aa
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-02-19 11:55:07 +1100

    feat: silent key treewalk(is lazy default to true? just make sure)

--- nvim/lua/ergou/plugins/treewalker.lua
+++ nvim/lua/ergou/plugins/treewalker.lua
@@ -5,15 +5,15 @@ return {
   ---Finally the arrow keys have a good mapping
   keys = {
     -- movement
-    { '<Up>', '<cmd>Treewalker Up<cr>', desc = 'Tree walk up' },
-    { '<Down>', '<cmd>Treewalker Down<cr>', desc = 'Tree walk down' },
-    { '<Left>', '<cmd>Treewalker Left<cr>', desc = 'Tree walk left' },
-    { '<Right>', '<cmd>Treewalker Right<cr>', desc = 'Tree walk right' },
+    { '<Up>', '<cmd>Treewalker Up<cr>', desc = 'Tree walk up', silent = true },
+    { '<Down>', '<cmd>Treewalker Down<cr>', desc = 'Tree walk down', silent = true },
+    { '<Left>', '<cmd>Treewalker Left<cr>', desc = 'Tree walk left', silent = true },
+    { '<Right>', '<cmd>Treewalker Right<cr>', desc = 'Tree walk right', silent = true },
 
     -- swapping
-    { '<M-Up>', '<cmd>Treewalker SwapUp<cr>', desc = 'Tree swap up' },
-    { '<M-Down>', '<cmd>Treewalker SwapDown<cr>', desc = 'Tree swap down' },
-    { '<M-Left>', '<cmd>Treewalker SwapLeft<cr>', desc = 'Tree swap left' },
-    { '<M-Right>', '<cmd>Treewalker SwapRight<cr>', desc = 'Tree swap right' },
+    { '<M-Up>', '<cmd>Treewalker SwapUp<cr>', desc = 'Tree swap up', silent = true },
+    { '<M-Down>', '<cmd>Treewalker SwapDown<cr>', desc = 'Tree swap down', silent = true },
+    { '<M-Left>', '<cmd>Treewalker SwapLeft<cr>', desc = 'Tree swap left', silent = true },
+    { '<M-Right>', '<cmd>Treewalker SwapRight<cr>', desc = 'Tree swap right', silent = true },
   },
 }
