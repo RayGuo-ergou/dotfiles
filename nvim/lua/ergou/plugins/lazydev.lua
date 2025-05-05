36c8762dbc8a7897b0769feec49a035810efc2cc
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-05-05 10:00:39 +1000

    feat: remove uv from lazydev as neovim added the type declarition

--- nvim/lua/ergou/plugins/lazydev.lua
+++ nvim/lua/ergou/plugins/lazydev.lua
@@ -4,7 +4,6 @@ return {
     ft = 'lua', -- only load on lua files
     opts = {
       library = {
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
         { path = 'nvim-lspconfig/lua', words = { 'lsp' } },
         { path = 'markdown.nvim/lua/render-markdown', words = { 'render%.md' } },
         { path = 'snacks.nvim', words = { 'Snacks' } },
