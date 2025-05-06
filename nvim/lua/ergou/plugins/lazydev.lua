16fa13b7ab6b05320690580eb569eb90c991a8f6
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-05-06 10:28:59 +1000

    feat: add wezterm for lazydev

--- nvim/lua/ergou/plugins/lazydev.lua
+++ nvim/lua/ergou/plugins/lazydev.lua
@@ -2,8 +2,12 @@ return {
   {
     'folke/lazydev.nvim',
     ft = 'lua', -- only load on lua files
+    dependencies = {
+      { 'gonstoll/wezterm-types', lazy = true },
+    },
     opts = {
       library = {
+        { path = 'wezterm-types', mods = { 'wezterm' } },
         { path = 'nvim-lspconfig/lua', words = { 'lsp' } },
         { path = 'markdown.nvim/lua/render-markdown', words = { 'render%.md' } },
         { path = 'snacks.nvim', words = { 'Snacks' } },
