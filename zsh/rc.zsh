48e6a6ffa392e7b51fdba6b08483812f216f3d92
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-05-20 14:35:56 +1000

    feat: source user-dirs.dirs if exist

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -14,6 +14,7 @@ source $DOTFILES/zsh/utils.zsh
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
+source_if_exists $HOME/.config/user-dirs.dirs
 source_if_exists $DOTFILES/zsh/p10k.zsh
 source_if_exists $DOTFILES/zsh/exports.zsh
 source_if_exists $ZSH/oh-my-zsh.sh
