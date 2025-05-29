123c9b32c7f02fde890939f9ed4477bce28d3c05
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-05-29 12:55:09 +1000

    feat: rename command for toggle auto formation

--- nvim/lua/ergou/config/cmds.lua
+++ nvim/lua/ergou/config/cmds.lua
@@ -1,5 +1,5 @@
 -- TODO: use snacks toggle
-vim.api.nvim_create_user_command('AutoFormattingToggle', function()
+vim.api.nvim_create_user_command('FormattingAutoToggle', function()
   vim.g.autoformat_enabled = not vim.g.autoformat_enabled
   local status = vim.g.autoformat_enabled and 'enabled' or 'disabled'
   vim.notify('Auto formatting ' .. status, vim.log.levels.INFO)
