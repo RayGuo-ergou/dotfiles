d85a815525ae2e0e55bf7c14879431f21c89d50f
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-06-05 21:54:12 +1000

    chore: remove wezterm type

--- nvim/lua/ergou/plugins/lazydev.lua
+++ nvim/lua/ergou/plugins/lazydev.lua
@@ -2,12 +2,8 @@ return {
   {
     'folke/lazydev.nvim',
     ft = 'lua', -- only load on lua files
-    dependencies = {
-      { 'gonstoll/wezterm-types', lazy = true },
-    },
     opts = {
       library = {
-        { path = 'wezterm-types', mods = { 'wezterm' } },
         { path = 'nvim-lspconfig/lua', words = { 'lsp' } },
         { path = 'markdown.nvim/lua/render-markdown', words = { 'render%.md' } },
         { path = 'snacks.nvim', words = { 'Snacks' } },
