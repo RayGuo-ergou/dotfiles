4978a38817fe2121e4e1885c38d0fc38592db1ad
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-07-01 13:53:06 +1000

    chroe: i give up use my own fork

--- nvim/lua/ergou/plugins/nvim-cmp.lua
+++ nvim/lua/ergou/plugins/nvim-cmp.lua
@@ -4,8 +4,7 @@ return {
     event = { 'InsertEnter', 'CmdlineEnter' },
     dependencies = {
       'hrsh7th/cmp-buffer', -- source for text in buffer
-      -- The alias code is poorly implemented and no testing
-      { 'hrsh7th/cmp-path', commit = '91ff86c' }, -- source for file system paths
+      'RayGuo-ergou/cmp-path', -- source for file system paths
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-cmdline',
       'hrsh7th/cmp-calc',
