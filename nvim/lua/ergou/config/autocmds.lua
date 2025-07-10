7c010c8d4602d351583ab4b44ddb0227c59b278f
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-07-10 17:36:26 +1000

    feat: relative number on differernt mode

--- nvim/lua/ergou/config/autocmds.lua
+++ nvim/lua/ergou/config/autocmds.lua
@@ -195,3 +195,30 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.comments = vim.opt_local.comments + '://'
   end,
 })
+
+---@see source https://github.com/sitiom/nvim-numbertoggle
+---@license https://github.com/sitiom/nvim-numbertoggle/blob/main/LICENSE
+local num_toggle_augroup = augroup('numbertoggle')
+vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'CmdlineLeave', 'WinEnter' }, {
+  pattern = '*',
+  group = num_toggle_augroup,
+  callback = function()
+    if vim.o.nu and vim.api.nvim_get_mode().mode ~= 'i' then
+      vim.opt.relativenumber = true
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'CmdlineEnter', 'WinLeave' }, {
+  pattern = '*',
+  group = num_toggle_augroup,
+  callback = function()
+    if vim.o.nu then
+      vim.opt.relativenumber = false
+      -- Conditional taken from https://github.com/rockyzhang24/dotfiles/commit/03dd14b5d43f812661b88c4660c03d714132abcf
+      -- Workaround for https://github.com/neovim/neovim/issues/32068
+      if not vim.tbl_contains({ '@', '-' }, vim.v.event.cmdtype) then
+        vim.cmd('redraw')
+      end
+    end
+  end,
+})
