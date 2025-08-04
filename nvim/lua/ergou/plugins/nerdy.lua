b66d1700d97d19faeb2282b903cbbf2a3fcc3824
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-08-04 16:41:03 +1000

    feat: nerdy change

--- nvim/lua/ergou/plugins/nerdy.lua
+++ nvim/lua/ergou/plugins/nerdy.lua
@@ -1,8 +1,8 @@
 return {
   '2kabhishek/nerdy.nvim',
   dependencies = {
-    'stevearc/dressing.nvim',
-    'nvim-telescope/telescope.nvim',
+    'folke/snacks.nvim',
   },
   cmd = 'Nerdy',
+  opts = {},
 }
