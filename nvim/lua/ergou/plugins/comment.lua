ff493c08146b7cebf4df7732d7cc11b5a40ebadc
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-08-07 21:18:31 +1000

    fea: use ts-comments.nvim

--- nvim/lua/ergou/plugins/comment.lua
+++ nvim/lua/ergou/plugins/comment.lua
@@ -1,28 +1,37 @@
 return {
-  'numToStr/Comment.nvim',
-  event = 'LazyFile',
-  opts = {
-    pre_hook = function()
-      local ft = require('Comment.ft')
-      if vim.bo.filetype == 'blade' then
-        -- make html comment {{--%s--}}
-        ft.set('html', { '{{--%s--}}', '{{--%s--}}' })
-      else
-        -- else fallback to default html comment <!--%s-->
-        ft.set('html', { '<!--%s-->', '<!--%s-->' })
-      end
-    end,
-  },
-  config = function(_, opts)
-    -- local ft = require('Comment.ft')
-    -- blade uses php comment for none html part
-    -- ft.set('blade', ft.get('php') or { '//%s', '/*%s*/' })
-
-    require('Comment').setup(opts)
-  end,
-  keys = {
-    {
-      'gcc',
+  {
+    enabled = false,
+    'numToStr/Comment.nvim',
+    event = 'LazyFile',
+    opts = {
+      pre_hook = function()
+        local ft = require('Comment.ft')
+        if vim.bo.filetype == 'blade' then
+          -- make html comment {{--%s--}}
+          ft.set('html', { '{{--%s--}}', '{{--%s--}}' })
+        else
+          -- else fallback to default html comment <!--%s-->
+          ft.set('html', { '<!--%s-->', '<!--%s-->' })
+        end
+      end,
     },
+    config = function(_, opts)
+      -- local ft = require('Comment.ft')
+      -- blade uses php comment for none html part
+      -- ft.set('blade', ft.get('php') or { '//%s', '/*%s*/' })
+
+      require('Comment').setup(opts)
+    end,
+    keys = {
+      {
+        'gcc',
+      },
+    },
+  },
+  {
+    'folke/ts-comments.nvim',
+    url = 'https://github.com/RayGuo-ergou/ts-comments.nvim',
+    opts = {},
+    event = 'LazyFile',
   },
 }
