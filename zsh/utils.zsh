4d9f418ac7d1c7cb70cdd6a1c6d9736598adf71b
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-10-06 16:51:18 +1100

    chore: silence

--- zsh/utils.zsh
+++ zsh/utils.zsh
@@ -1,8 +1,8 @@
 execute_if_command_exists() {
     if command -v "$1" >/dev/null 2>&1; then
         eval "$2"
-    else
-        echo "Command not found: $1"
+    # else
+    #     echo "Command not found: $1"
     fi
 }
 
