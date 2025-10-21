e10233caaad55a1707346c79eab0d0c966eb0fdc
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-10-21 16:44:23 +1100

    feat: blade fixed TS query, use folke ts-comments plugin

--- nvim/lua/ergou/plugins/comment.lua
+++ nvim/lua/ergou/plugins/comment.lua
@@ -30,8 +30,19 @@ return {
   },
   {
     'folke/ts-comments.nvim',
-    url = 'https://github.com/RayGuo-ergou/ts-comments.nvim',
-    opts = {},
+    opts = {
+      lang = {
+        blade = {
+          '{{-- %s --}}',
+          program = '// %s',
+          php_statement = '// %s',
+        },
+        vue = {
+          '<!-- %s -->',
+          script_element = '// %s',
+        },
+      },
+    },
     event = 'LazyFile',
   },
 }
