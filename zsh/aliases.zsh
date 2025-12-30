1123e70329d8f874e91284a5eaecb4c94d465e98
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-12-31 09:40:18 +1100

    feat: alias to disable/enable laptop screen

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -28,6 +28,8 @@ alias jourwr='journalctl -p warning..emerg -r'
 alias rmorp='sudo pacman -R $(pacman -Qdtq)'
 alias laltf='php artisan test --filter'
 alias lalcc='php artisan optimize:clear'
+alias liddown='hyprctl keyword monitor "eDP-1,disable"'
+alias lidup='hyprctl keyword monitor "eDP-1,highrr,auto,1"'
 
 vimf() {
   local file=$(fd --type f --hidden --exclude .git | fzf --reverse)
