c745c5128cc579a3e55ed88eba0847cb16ef3be7
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2026-01-09 09:08:47 +1100

    feat: move machine specific configs to local file

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -27,8 +27,6 @@ alias jourwr='journalctl -p warning..emerg -r'
 # see https://forum.endeavouros.com/t/how-to-delete-orphaned-packages-pacman-vs-pamac/45218
 alias rmorp='sudo pacman -R $(pacman -Qdtq)'
 alias lalcc='php artisan optimize:clear'
-alias liddown='hyprctl keyword monitor "eDP-1,disable"'
-alias lidup='hyprctl keyword monitor "eDP-1,highrr,auto,1"'
 alias weather='curl wttr.in'
 
 vimf() {
