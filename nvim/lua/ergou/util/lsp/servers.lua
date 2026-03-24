3c017e8204e47dbc6a35b1270aaba3ffa5ed88f5
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2026-03-25 10:14:10 +1100

    feat: disable text docuemnt formatting for yamlls

--- nvim/lua/ergou/util/lsp/servers.lua
+++ nvim/lua/ergou/util/lsp/servers.lua
@@ -200,6 +200,12 @@ M.get = function()
           schemas = require('schemastore').yaml.schemas(),
         },
       },
+      capabilities = {
+        textDocument = {
+          ---@diagnostic disable-next-line: assign-type-mismatch if assign nil for some reason it does not work
+          formatting = false,
+        },
+      },
       on_attach = function(client, _)
         -- FIXME: only disable if eslint attached
         client.server_capabilities.documentFormattingProvider = nil
